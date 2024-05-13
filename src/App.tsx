6f66a87fdac086ba48e00d05b15ec793dc82a7b1
trevorbaron <trevorbaron@bytedance.com>
2024-05-13 13:59:23 -0700

    react tab state sync working

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
