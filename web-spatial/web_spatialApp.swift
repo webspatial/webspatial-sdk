8cf403450e9ba8d1dcb62bde757bdb06012ca841
trevorbaron <trevorbaron@bytedance.com>
2024-05-13 15:14:56 -0700

    populate window.WebSpatailEnabled, move to single index.html for root

--- web-spatial/web_spatialApp.swift
+++ web-spatial/web_spatialApp.swift
@@ -27,7 +27,7 @@ struct web_spatialApp: App {
     init() {
         print("WebSpatial App Started --------")
 
-        root = wgManager.createWebView(windowGroup: "root", windowID: "root", url: URL(string: "http://npmURL:5173/")!)
+        root = wgManager.createWebView(windowGroup: "root", windowID: "root", url: URL(string: "http://npmURL:5173/http://npmURL:5173/?pageName=helloWorldApp/main.tsx")!)
         rootWGD = wgManager.getWindowGroup(windowGroup: "root")
     }
 
