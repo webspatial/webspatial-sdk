c971f93f34b441c7b9a20dd255615905b0c96a88
trevorbaron <trevorbaron@bytedance.com>
2024-05-17 16:10:47 -0700

    refactor components to lib folder

--- src/reactDemo/basic.tsx
+++ src/reactDemo/basic.tsx
@@ -5,13 +5,23 @@ import WebSpatial from '../../lib/webSpatial'
 import ReactDomServer from 'react-dom/server';
 
 (window as any).wx = WebSpatial
+
+var _panelContentUpdate = "";
+(window as any).updatePanelContent = (str: string) => {
+    _panelContentUpdate = str;
+    if ((window as any).myContentUpdate) {
+        (window as any).myContentUpdate(_panelContentUpdate)
+    }
+}
+
 function App() {
     const myDiv = useRef(null);
     useEffect(() => {
-        (window as any).updatePanelContent = (str: string) => {
+        (window as any).myContentUpdate = (str: string) => {
             (myDiv.current! as HTMLElement).innerHTML = str
         }
-    })
+        (window as any).myContentUpdate(_panelContentUpdate)
+    }, [])
     return (
         <div className='w-screen h-screen' ref={myDiv}>
 
