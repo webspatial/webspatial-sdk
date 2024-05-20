9351a983f712252c0043c148a54c6bb29d667279
trevorbaron <trevorbaron@bytedance.com>
2024-05-20 14:46:54 -0700

    spatial model component, send back resize complete event

--- lib/webSpatialComponents.tsx
+++ lib/webSpatialComponents.tsx
@@ -60,3 +60,49 @@ export function SpatialDiv(props: { webViewID: string, className: string, childr
         </div>
     )
 }
+
+
+export function SpatialModel(props: { webViewID: string, className: string, children?: ReactElement | Array<ReactElement>, spatialOffset?: { x?: number, y?: number, z?: number } }) {
+    props = { ...{ spatialOffset: { x: 0, y: 0, z: 0 } }, ...props }
+    if (props.spatialOffset!.x === undefined) {
+        props.spatialOffset!.x = 0
+    }
+    if (props.spatialOffset!.y === undefined) {
+        props.spatialOffset!.y = 0
+    }
+    if (props.spatialOffset!.z === undefined) {
+        props.spatialOffset!.z = 0
+    }
+
+    const myDiv = useRef(null);
+    useEffect(() => {
+        if (!(window as any).WebSpatailEnabled) {
+            return
+        }
+
+        var resizeDiv = async () => {
+            var element = (myDiv.current! as HTMLElement)
+            var rect = element.getBoundingClientRect();
+            var curPosX = (rect.left + ((rect.right - rect.left) / 2))
+            var curPosY = (rect.bottom + ((rect.top - rect.bottom) / 2)) + window.scrollY
+            await WebSpatial.updateDOMModelPosition("root", "root", props.webViewID, { x: curPosX, y: curPosY, z: props.spatialOffset!.z! })
+        }
+        var setContent = async () => {
+            await WebSpatial.createDOMModel("root", "root", props.webViewID, "http://testIP:5173/src/assets/FlightHelmet.usdz")
+            await resizeDiv()
+        }
+        setContent()
+
+        addEventListener("resize", resizeDiv);
+
+        return () => {
+            removeEventListener("resize", resizeDiv)
+        }
+    }, [])
+
+    return (
+        <div ref={myDiv} className={props.className}>
+            {((window as any).WebSpatailEnabled) ? <div /> : props.children}
+        </div>
+    )
+}
