a5ada637f3c038f5b2ef0115691298e89acfd46f
trevorbaron <trevorbaron@bytedance.com>
2024-05-29 13:32:54 -0700

    always full reload on save

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const fullReloadAlways: PluginOption = {
+  name: 'full-reload-always',
+  handleHotUpdate({ server }) {
+    server.hot.send({ type: "full-reload" })
+    return []
+  },
+} as PluginOption
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), fullReloadAlways],
 })
