dde1dad23f9fa4de23d13d2edc7938a74cdfed23
trevorbaron <trevorbaron@bytedance.com>
2024-05-31 16:45:36 -0700

    add tests: glass background, standard webpage

--- src/jsApiTestPages/tests.ts
+++ src/jsApiTestPages/tests.ts
@@ -4,16 +4,25 @@ var main = async()=>{
     const urlParams = new URLSearchParams(window.location.search);
     var page = urlParams.get("pageName")
     page = (page ? page : "default")
-    await WebSpatial.log("Page loaded: "+page)
+    await WebSpatial.log("        --------------Page loaded: "+page)
 
     if(page == "default"){
         await WebSpatial.log("Nothing to do")
     }else if(page == "webView"){
         await WebSpatial.log("Trying to load webview")
-        var panel = await WebSpatial.createWebPanel(WebSpatial.getCurrentWindowGroup(), "http://google.com")
-        await WebSpatial.updatePanelPose(WebSpatial.getCurrentWindowGroup(), panel, {x:300,y:300,z:300}, 300, 300)
+        var panel = await WebSpatial.createWebPanel(WebSpatial.getCurrentWindowGroup(), "http://testIP:5173/testList.html")
+        await WebSpatial.updatePanelPose(WebSpatial.getCurrentWindowGroup(), panel, {x:700,y:300,z:300}, 300, 300)
         await WebSpatial.log("Create complete")
-    }   
+        setTimeout(async () => {
+            await WebSpatial.destroyWebPanel(WebSpatial.getCurrentWindowGroup(), panel)
+            await WebSpatial.log("destroy complete")
+        }, 2000);
+    }else if(page == "glassBackground"){
+        await WebSpatial.setWebPanelStyle(WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel())
+        document.documentElement.style.backgroundColor = "transparent";
+        document.body.style.backgroundColor = "transparent"
+        await WebSpatial.log("set to glass background")
+    }
 }
 main()
 
