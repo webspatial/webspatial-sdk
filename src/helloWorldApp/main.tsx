7966c185b6d0aebc03def5d7409ba1245d9d937d
trevorbaron <trevorbaron@bytedance.com>
2024-06-12 16:00:28 -0700

    update iframe componet to support public api

--- src/helloWorldApp/main.tsx
+++ src/helloWorldApp/main.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import '../index.css'
-import { WindowGroup } from "../../lib/webSpatial.ts"
 import hnLogo from './assets/y18.svg'
-import { SpatialIFrame } from '../../lib/webSpatialComponents'
+import { SpatialIFrame, getSessionAsync } from '../../lib/webSpatialComponents'
 
 import { Provider } from 'react-redux'
 import { initMessageListener } from 'redux-state-sync';
@@ -11,14 +10,12 @@ import { useDispatch, useSelector } from "react-redux";
 import store, { increment } from "./store.ts"
 initMessageListener(store);
 
-import WebSpatial from "../../lib/webSpatial.ts"
-
 function App() {
   const [count, setCount] = useState(0)
   const sharedCount = useSelector((state: any) => state.count.value);
   const dispatch = useDispatch()
   const [created, setCreated] = useState(false)
-  var volumetricWG = new WindowGroup()
+  //var volumetricWG = new WindowGroup()
   //var volumetricPanel = new WebPanel()
 
 
@@ -49,25 +46,25 @@ function App() {
           <h1>Toggle Immersive</h1>
           <label className="inline-flex items-center cursor-pointer">
             <input type="checkbox" value="" className="sr-only peer" onChange={async (e) => {
-              if (e.target.checked) {
-                await WebSpatial.openImmersiveSpace()
-                if (!created) {
-                  setCreated(true)
+              // if (e.target.checked) {
+              //   await WebSpatial.openImmersiveSpace()
+              //   if (!created) {
+              //     setCreated(true)
 
-                  let helmetModel = await WebSpatial.createResource("ModelComponent", WebSpatial.getImmersiveWindowGroup(), { modelURL: "http://testIP:5173/src/assets/FlightHelmet.usdz" });
-                  var ent = await WebSpatial.createEntity(WebSpatial.getImmersiveWindowGroup())
-                  WebSpatial.setComponent(ent, helmetModel)
-                  WebSpatial.onFrame(async (x: number) => {
+              //     let helmetModel = await WebSpatial.createResource("ModelComponent", WebSpatial.getImmersiveWindowGroup(), { modelURL: "http://testIP:5173/src/assets/FlightHelmet.usdz" });
+              //     var ent = await WebSpatial.createEntity(WebSpatial.getImmersiveWindowGroup())
+              //     WebSpatial.setComponent(ent, helmetModel)
+              //     WebSpatial.onFrame(async (x: number) => {
 
-                    ent.position.x = Math.sin(x / 1000)
-                    ent.position.y = 1.5
-                    ent.position.z = -1
-                    WebSpatial.updateEntityPose(ent)
-                  })
-                }
-              } else {
-                await WebSpatial.dismissImmersiveSpace()
-              }
+              //       ent.position.x = Math.sin(x / 1000)
+              //       ent.position.y = 1.5
+              //       ent.position.z = -1
+              //       WebSpatial.updateEntityPose(ent)
+              //     })
+              //   }
+              // } else {
+              //   await WebSpatial.dismissImmersiveSpace()
+              // }
             }}></input>
             <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           </label>
@@ -76,17 +73,17 @@ function App() {
           <h1>Open Volumetric</h1>
           <button className="select-none px-4 py-1 text-s font-semibold rounded-full border border-gray-700 hover:text-white bg-gray-700 hover:bg-gray-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
             onClick={async (e) => {
-              if (volumetricWG.id === "") {
-                volumetricWG = await WebSpatial.createWindowGroup("Volumetric")
-                await new Promise(resolve => setTimeout(resolve, 500));
-                // volumetricPanel = await WebSpatial.createWebPanel(volumetricWG, "/index.html?pageName=helloWorldApp/main2.tsx")
-                // await WebSpatial.updatePanelPose(volumetricWG, volumetricPanel, { x: 0, y: 0, z: -0.4 }, 1920, 1080)
-                // await WebSpatial.createMesh(volumetricWG, "myMesh")
-                // await WebSpatial.createDOMModel(volumetricWG, volumetricPanel, "testModel", "http://testIP:5173/src/assets/FlightHelmet.usdz")
-                // setTimeout(() => {
-                //   WebSpatial.updateDOMModelPosition(volumetricWG, volumetricPanel, "testModel", { x: 0, y: -0.5, z: 0 })
-                // }, 3000);
-              }
+              // if (volumetricWG.id === "") {
+              //   volumetricWG = await WebSpatial.createWindowGroup("Volumetric")
+              //   await new Promise(resolve => setTimeout(resolve, 500));
+              //   // volumetricPanel = await WebSpatial.createWebPanel(volumetricWG, "/index.html?pageName=helloWorldApp/main2.tsx")
+              //   // await WebSpatial.updatePanelPose(volumetricWG, volumetricPanel, { x: 0, y: 0, z: -0.4 }, 1920, 1080)
+              //   // await WebSpatial.createMesh(volumetricWG, "myMesh")
+              //   // await WebSpatial.createDOMModel(volumetricWG, volumetricPanel, "testModel", "http://testIP:5173/src/assets/FlightHelmet.usdz")
+              //   // setTimeout(() => {
+              //   //   WebSpatial.updateDOMModelPosition(volumetricWG, volumetricPanel, "testModel", { x: 0, y: -0.5, z: 0 })
+              //   // }, 3000);
+              // }
 
 
 
@@ -176,7 +173,8 @@ function App() {
   )
 }
 
-WebSpatial.updateResource(WebSpatial.getCurrentWebPanel(), { style: { glassEffect: true, cornerRadius: 50 } })
+// WebSpatial.updateResource(WebSpatial.getCurrentWebPanel(), { style: { glassEffect: true, cornerRadius: 50 } })
+await (await getSessionAsync()).getCurrentIFrameComponent().setStyle({ glassEffect: true, cornerRadius: 50 })
 document.documentElement.style.backgroundColor = "transparent";
 document.body.style.backgroundColor = "transparent"
 ReactDOM.createRoot(document.getElementById('root')!).render(
