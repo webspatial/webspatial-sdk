dcde7ecd7d270a518c2c1dd4315c4f83f60a2e8b
trevorbaron <trevorbaron@bytedance.com>
2024-06-12 10:38:21 -0700

    major refactor part 2

--- src/jsApiTestPages/tests.ts
+++ src/jsApiTestPages/tests.ts
@@ -1,5 +1,5 @@
 import { Euler, Quaternion, Vector3 } from 'three';
-import WebSpatial, { SpatialEntity } from '../../lib/webSpatial'
+import WebSpatial, { SpatialResource, Vec3, Vec4 } from '../../lib/webSpatial'
 
 var main = async () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -11,13 +11,15 @@ var main = async () => {
         await WebSpatial.log("Nothing to do")
     } else if (page == "webView") {
         await WebSpatial.log("Trying to load webview")
-        let entity = await WebSpatial.createEntity();
-        entity.position.x = 700
-        entity.position.y = 300
-        entity.position.z = 300
-        WebSpatial.updateEntityPose(entity)
+        let entity = await WebSpatial.createResource("Entity", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel());
+        await WebSpatial.log("create complete")
+        entity.data.position = new Vec3(700, 300, 300)
+        entity.data.orientation = new Vec4()
+        entity.data.scale = new Vec3(1, 1, 1)
+        await WebSpatial.log("Trying to update resource")
+        WebSpatial.updateResource(entity)
 
-        let webview = await WebSpatial.createResource("SpatialWebView");
+        let webview = await WebSpatial.createResource("SpatialWebView", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel());
         webview.data.url = "http://10.73.196.42:5173/testList.html"
         webview.data.resolution = { x: 300, y: 300 }
         webview.data.inline = true
@@ -26,7 +28,7 @@ var main = async () => {
 
         await WebSpatial.log("created webview")
         setTimeout(async () => {
-            await WebSpatial.destroyEntity(entity)
+            await WebSpatial.destroyResource(entity)
             await WebSpatial.log("destroy complete")
         }, 2000);
     } else if (page == "glassBackground") {
@@ -37,25 +39,25 @@ var main = async () => {
         await WebSpatial.log("set to glass background")
     } else if (page == "model") {
         WebSpatial.log("create entitys")
-        let ballModel = await WebSpatial.createResource("ModelComponent", { modelURL: "http://10.73.196.42:5173/src/assets/ball.usdz" });
-        let helmetModel = await WebSpatial.createResource("ModelComponent", { modelURL: "http://10.73.196.42:5173/src/assets/FlightHelmet.usdz" });
+        let ballModel = await WebSpatial.createResource("ModelComponent", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel(), { modelURL: "http://10.73.196.42:5173/src/assets/ball.usdz" });
+        let helmetModel = await WebSpatial.createResource("ModelComponent", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel(), { modelURL: "http://10.73.196.42:5173/src/assets/FlightHelmet.usdz" });
 
-        var entities = new Array<{ e: SpatialEntity, v: number }>()
+        var entities = new Array<{ e: SpatialResource, v: number }>()
 
         for (var i = 0; i < 7; i++) {
-            let entity = await WebSpatial.createEntity();
-            entity.position.x = -0.35 + (i * 0.1)
-            entity.position.z = 0.2 + 0.00001 * i
-            entity.scale = { x: 0.07, y: 0.07, z: 0.07 }
-            WebSpatial.updateEntityPose(entity)
+            let entity = await WebSpatial.createResource("Entity", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel());
+            entity.data.position = new Vec3(-0.35 + (i * 0.1), 0, 0.2 + 0.00001 * i)
+            entity.data.orientation = new Vec4()
+            entity.data.scale = new Vec3(0.07, 0.07, 0.07)
+            WebSpatial.updateResource(entity)
 
-            let mesh = await WebSpatial.createResource("MeshResource", { shape: Math.random() < 0.3 ? "sphere" : "box" });
-            let material = await WebSpatial.createResource("PhysicallyBasedMaterial");
+            let mesh = await WebSpatial.createResource("MeshResource", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel(), { shape: Math.random() < 0.3 ? "sphere" : "box" });
+            let material = await WebSpatial.createResource("PhysicallyBasedMaterial", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel());
             material.data.baseColor = { r: Math.random(), g: Math.random() * 0.3, b: Math.random() * 0.3, a: 1.0 }
             material.data.metallic = { value: Math.random() * 0.3 }
             material.data.roughness = { value: Math.random() }
             WebSpatial.updateResource(material)
-            let modelComponent = await WebSpatial.createResource("ModelComponent");
+            let modelComponent = await WebSpatial.createResource("ModelComponent", WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel());
             modelComponent.data.meshResource = mesh.id
             modelComponent.data.materials = [material.id]
             WebSpatial.updateResource(modelComponent)
@@ -88,18 +90,18 @@ var main = async () => {
             for (var i = 0; i < entities.length; i++) {
                 var entity = entities[i].e
                 entities[i].v -= 5 * (dt / 1000)
-                entity.position.y += (dt / 1000) * entities[i].v
-                if (entity.position.y < floor) {
-                    entity.position.y = floor
+                entity.data.position.y += (dt / 1000) * entities[i].v
+                if (entity.data.position.y < floor) {
+                    entity.data.position.y = floor
                     entities[i].v = -entities[i].v * 0.5
                 }
                 q.setFromEuler(new Euler(0, time / 1000, 0))
-                entity.orientation.x = q.x
-                entity.orientation.y = q.y
-                entity.orientation.z = q.z
-                entity.orientation.w = q.w
-                entity.scale.y = (Math.pow(((Math.sin(time / 100) + 1) / 2), 5) * 0.02) + 0.07 // 0.07 * (Math.abs((entities[i].v / 2)) + 1)
-                WebSpatial.updateEntityPose(entity)
+                entity.data.orientation.x = q.x
+                entity.data.orientation.y = q.y
+                entity.data.orientation.z = q.z
+                entity.data.orientation.w = q.w
+                entity.data.scale.y = (Math.pow(((Math.sin(time / 100) + 1) / 2), 5) * 0.02) + 0.07 // 0.07 * (Math.abs((entities[i].v / 2)) + 1)
+                WebSpatial.updateResource(entity)
             }
 
         })
