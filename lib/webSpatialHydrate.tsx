1c84c0a7c6cf96c2e7c675dfc7d2d14eb513b6da
trevorbaron <trevorbaron@bytedance.com>
2024-06-12 10:38:16 -0700

    major refactor part 1

--- lib/webSpatialHydrate.tsx
+++ lib/webSpatialHydrate.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { SpatialDiv, SpatialModel } from './webSpatialComponents'
+import { SpatialIFrame } from './webSpatialComponents'
 
 var elements = document.querySelectorAll("[custom-spatial]")
 var wvCounter = 0
 for (var e of elements) {
     ReactDOM.createRoot(e).render(
         <React.StrictMode>
-            <SpatialDiv spatialOffset={{ z: 50 }} className='' webViewID={"customWV" + (wvCounter++)}>
-                <div dangerouslySetInnerHTML={{ __html: e.innerHTML }}></div>
-            </SpatialDiv>
+            <SpatialIFrame spatialOffset={{ z: 50 }} className='' src="google.com">
+                {/* <div dangerouslySetInnerHTML={{ __html: e.innerHTML }}></div> */}
+            </SpatialIFrame>
         </React.StrictMode >,
     )
 }
