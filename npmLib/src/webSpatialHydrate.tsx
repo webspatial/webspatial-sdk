a926ac78b95ebeeafe3ca1de478dfad154e8da4f
trevorbaron <trevorbaron@bytedance.com>
2024-06-21 15:57:58 -0700

    refactoring embeded

--- npmLib/src/webSpatialHydrate.tsx
+++ npmLib/src/webSpatialHydrate.tsx
@@ -1,15 +1,33 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
-import { SpatialIFrame } from './webSpatialComponents'
+import { SpatialIFrame, getSessionAsync } from './webSpatialComponents'
+import { Spatial } from '.'
 
-var elements = document.querySelectorAll("[custom-spatial]")
-var wvCounter = 0
-for (var e of elements) {
-    ReactDOM.createRoot(e).render(
-        <React.StrictMode>
-            <SpatialIFrame spatialOffset={{ z: 50 }} className='' src="google.com">
-                {/* <div dangerouslySetInnerHTML={{ __html: e.innerHTML }}></div> */}
-            </SpatialIFrame>
-        </React.StrictMode >,
-    )
+export class WebSpatialHydrate {
+    static async Hydrate() {
+
+        if (!new Spatial().isSupported()) {
+            console.warn("Webspatial not supported")
+            return
+        }
+
+        var session = await getSessionAsync()
+        await session.getCurrentIFrameComponent().setStyle({ glassEffect: true, cornerRadius: 50 })
+        // await WebSpatial.setWebPanelStyle(WebSpatial.getCurrentWindowGroup(), WebSpatial.getCurrentWebPanel())
+        document.documentElement.style.backgroundColor = "transparent";
+        document.body.style.backgroundColor = "transparent"
+
+        var elements = document.querySelectorAll("[custom-spatial]")
+        for (var e of elements) {
+            let toSet = encodeURIComponent(e.innerHTML)
+            ReactDOM.createRoot(e).render(
+                <React.StrictMode>
+                    <SpatialIFrame spatialOffset={{ z: 80 }} style={{ width: e.clientWidth, height: e.clientHeight }} className='' src="/bootstrapEmbed.html" onload={(spatialFrame) => {
+                        spatialFrame.sendContent(toSet)
+                    }}>
+                    </SpatialIFrame>
+                </React.StrictMode >,
+            )
+        }
+    }
 }
