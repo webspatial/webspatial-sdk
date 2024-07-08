dff5e7cd70d081bd1cb67bc2835aee7835967373
trevorbaron <trevorbaron@bytedance.com>
2024-07-08 15:49:11 -0700

    add daisyUI components

--- webTests/tailwind.config.js
+++ webTests/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,6 +10,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
 
