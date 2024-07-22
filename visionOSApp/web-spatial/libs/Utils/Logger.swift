2847ff02e44e6f1de92dc738e07b86d5de1ecaf9
王任 wangren <wangren.walter@bytedance.com>
2024-07-22 17:08:17 +0800

    refactor code according to CR

--- visionOSApp/web-spatial/libs/Utils/Logger.swift
+++ visionOSApp/web-spatial/libs/Utils/Logger.swift
@@ -8,24 +8,14 @@
 import Foundation
 import SwiftyBeaver
 
-struct Utils {
-    static func initUtils() {
-        Utils.initLogger();
-    }
-    
+struct Logger {
     static func getLogger() -> SwiftyBeaver.Type {
         return SwiftyBeaver.self;
     }
-
-    static func setLoggerLevel() -> SwiftyBeaver.Type {
-        return SwiftyBeaver.self;
-    }
-
     
-    private static func initLogger(){
+    static func initLogger(){
         var logConsole = ConsoleDestination();
         logConsole.useTerminalColors = true;
         SwiftyBeaver.self.addDestination(logConsole)
-        
     }
 }
