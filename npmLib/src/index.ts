5fda71040a9dbd33650c3c8a1815f1c840522766
王任 wangren <wangren.walter@bytedance.com>
2024-08-12 21:13:33 +0800

    refacter code

--- npmLib/src/index.ts
+++ npmLib/src/index.ts
@@ -1,15 +1,9 @@
 export { Spatial } from './Spatial'
 export {SpatialEntity} from './SpatialEntity'
 export {SpatialSession} from './SpatialSession'
-export {SpatialResource} from './SpatialResource'
-export {SpatialIFrameComponent} from './SpatialIFrameComponent'
-export {SpatialInputComponent} from './SpatialInputComponent'
-export {SpatialMeshResource} from './SpatialMeshResource'
-export {SpatialModelComponent} from './SpatialModelComponent'
-export {SpatialModelUIComponent} from './SpatialModelUIComponent'
-export {SpatialPhysicallyBasedMaterial} from './SpatialPhysicallyBasedMaterial'
 export {SpatialTransform} from './SpatialTransform'
 export {SpatialWindowGroup} from './SpatialWindowGroup'
+export * from './SpatialResource'
 
 import { WebSpatial } from './webSpatialPrivate'
 export const WebSpatialLogger = WebSpatial.logger
