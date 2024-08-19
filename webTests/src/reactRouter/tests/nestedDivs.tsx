639e7e1c44d58b756b1ff3df49fa286c18bba759
trevorbaron <trevorbaron@bytedance.com>
2024-08-19 16:36:30 -0700

    get reference to parent spatial div context

--- webTests/src/reactRouter/tests/nestedDivs.tsx
+++ webTests/src/reactRouter/tests/nestedDivs.tsx
@@ -2,10 +2,21 @@ import { SpatialDiv } from "web-spatial/src/webSpatialComponents"
 
 export function NestedDivsTest() {
     return <>
-        <SpatialDiv spatialStyle={{ position: { z: 1, x: 0, y: 0 }, glassEffect: true }} style={{ width: 500, height: 300, backgroundColor: "red" }}>
+        <SpatialDiv debugName="PARENT A ROOT" spatialStyle={{ position: { z: 1, x: 0, y: 0 }, glassEffect: true }} style={{ width: 500, height: 300, backgroundColor: "red" }}>
             <p>Hello world A</p>
-            <SpatialDiv spatialStyle={{ position: { z: 3, x: 0, y: 0 }, glassEffect: true }} style={{ width: 200, height: 100, backgroundColor: "blue" }}>
+            <SpatialDiv debugName="CHILD A1" spatialStyle={{ position: { z: 3, x: 0, y: 0 }, glassEffect: true }} style={{ width: 200, height: 100, backgroundColor: "blue" }}>
                 <p>Hello world B</p>
+            </SpatialDiv>
+        </SpatialDiv>
+
+
+        <SpatialDiv debugName="PARENT B ROOT" spatialStyle={{ position: { z: 1, x: 0, y: 0 }, glassEffect: true }} style={{ width: 500, height: 300, backgroundColor: "red" }}>
+            <p>Hello world A</p>
+            <SpatialDiv debugName="CHILD B1" spatialStyle={{ position: { z: 3, x: 0, y: 0 }, glassEffect: true }} style={{ width: 200, height: 100, backgroundColor: "blue" }}>
+                <p>Hello world B</p>
+                <SpatialDiv debugName="CHILD B2" spatialStyle={{ position: { z: 3, x: 0, y: 0 }, glassEffect: true }} style={{ width: 200, height: 100, backgroundColor: "blue" }}>
+                    <p>Hello world B</p>
+                </SpatialDiv>
             </SpatialDiv>
         </SpatialDiv>
     </>
