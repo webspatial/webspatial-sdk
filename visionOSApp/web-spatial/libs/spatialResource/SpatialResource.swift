bb575b159c435b4d8d0ca58284346b7ff029e19e
trevorbaron <trevorbaron@bytedance.com>
2024-08-19 14:52:00 -0700

    move SpatialResource to its own file

---
+++ visionOSApp/web-spatial/libs/spatialResource/SpatialResource.swift
@@ -0,0 +1,109 @@
+//
+//  SpatialResource.swift
+//  web-spatial
+//
+//  Created by ByteDance on 8/19/24.
+//
+
+import Combine
+import Foundation
+import RealityKit
+import typealias RealityKit.Entity
+import SwiftUI
+
+/**
+ * Resource created from JS. Can be an entity or object that is attached to an entity like a mesh
+ */
+class SpatialResource: WatchableObject, Component {
+    // Always populated
+    let id = UUID().uuidString
+    var resourceType = "undefined"
+
+    // populated based on type
+    var meshResource: MeshResource?
+    var physicallyBasedMaterial: PhysicallyBasedMaterial?
+    var modelComponent: ModelComponent?
+    @Published var modelUIComponent: ModelUIComponent?
+    @Published var inputComponent: InputComponent?
+    @Published var spatialWebView: SpatialWebView?
+    @Published var forceUpdate = false
+
+    // Entity
+    let modelEntity = ModelEntity()
+
+    // This resource will be destroyed if this webview is destroyed
+    weak var ownerWebview: SpatialWebView?
+
+    // Window group that this entity will be displayed in (not related to resource ownership)
+    weak var parentWindowGroup: WindowGroupContentDictionary?
+
+    func setParentWindowGroup(wg: WindowGroupContentDictionary?) {
+        if let g = parentWindowGroup {
+            g.childEntities.removeValue(forKey: id)
+        }
+        parentWindowGroup = wg
+        if let g = parentWindowGroup {
+            g.childEntities[id] = self
+        }
+    }
+
+    init(resourceType: String, mngr: WindowGroupManager, windowGroupID: String, owner: SpatialWebView?) {
+        self.resourceType = resourceType
+        self.ownerWebview = owner
+
+        super.init()
+        mngr.allResources[id] = self
+    }
+
+    func destroy() -> Bool {
+        var removed = false
+        if resourceType == "Entity" {
+            modelEntity.removeFromParent()
+            if let wg = parentWindowGroup {
+                if let _ = wg.childEntities.removeValue(forKey: id) {
+                    removed = true
+                }
+            }
+        }
+
+        if let wv = ownerWebview {
+            if let _ = wv.childResources.removeValue(forKey: id) {
+                removed = true
+            }
+        }
+
+        _ = wgManager.allResources.removeValue(forKey: id)
+        return removed
+    }
+}
+
+class ModelUIComponent: WatchableObject {
+    @Published var url: URL?
+    @Published var aspectRatio: String = "fit"
+    @Published var resolutionX: Double = 0
+    @Published var resolutionY: Double = 0
+
+    @Published var opacity = false
+
+    //  animation related function and props
+//    var animationEaseFn: AnimationEaseStyle  = .easeInOut
+    @Published var animateSubject = PassthroughSubject<AnimationDescription, Never>()
+
+    func triggerAnimation(_ animationDesc: AnimationDescription) {
+        animateSubject.send(animationDesc)
+    }
+
+//    called by PlainWindowGroupView
+    func onAnimation(_ animationDesc: AnimationDescription) {
+//        print("onAnimation triggered")
+        opacity = animationDesc.fadeOut
+    }
+}
+
+class InputComponent: WatchableObject {
+    weak var wv: SpatialWebView?
+    var itc = InputTargetComponent()
+    var resourceID = ""
+    var isDragging = false
+    var trackedPosition: SIMD3<Float> = .zero
+}
