e7f1277744db860af72660aebc8b1277962af978
trevorbaron <trevorbaron@bytedance.com>
2024-08-19 14:23:23 -0700

    nested divs test

--- webTests/src/reactRouter/index.tsx
+++ webTests/src/reactRouter/index.tsx
@@ -2,46 +2,8 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter, Link, Route, Routes } from 'react-router-dom'
 import { SpatialDiv } from 'web-spatial/src/webSpatialComponents'
-
-
-
-function PortalTest() {
-    return <>
-        <div>
-            To be implemented
-        </div>
-        <div>
-            <SpatialDiv spatialStyle={{ position: { z: 100, x: 0, y: 0 } }} style={{ position: "absolute", top: "45%", left: "45%", width: "10%", height: "10%", backgroundColor: "white" }}>
-                <p>This is a popup</p>
-            </SpatialDiv>
-            <div style={{ color: "red" }}>
-                <SpatialDiv spatialStyle={{ position: { z: 20, x: 0, y: 0 } }}>
-                    <p>This text should be red</p>
-                </SpatialDiv>
-            </div>
-            <SpatialDiv className='p-10' spatialStyle={{ position: { z: 50, x: 100, y: 50 } }} style={{ backgroundColor: "gray", width: "50%" }}>
-                <div onClick={() => { document.body.style.backgroundColor = 'gray' }}>
-                    <p>This text is inside a portal iframe</p>
-                    <a href="/">click me</a>
-                </div>
-            </SpatialDiv>
-            <div className='p-10' style={{ backgroundColor: "gray", width: "50%" }}>
-                <div onClick={() => { document.body.style.backgroundColor = 'gray' }}>
-                    <p>This text is inside a div notiframe</p>
-                </div>
-            </div>
-            <div className='p-10' >
-                <div onClick={() => { document.body.style.backgroundColor = 'gray' }}>
-                    <p>This text is inside a div notiframe</p>
-                </div>
-            </div>
-            <SpatialDiv className='p-10' spatialStyle={{ position: { z: 50, x: 0, y: 0 }, glassEffect: true, materialThickness: "thick" }} >
-                <p>Another spatial div</p>
-            </SpatialDiv>
-        </div>
-
-    </>
-}
+import { PortalTest } from './tests/portal'
+import { NestedDivsTest } from './tests/nestedDivs'
 
 function HomePage() {
     return <>
@@ -63,12 +25,14 @@ ReactDOM.createRoot(root).render(
                     <ul className="menu menu-horizontal px-1">
                         <li><a href="/src/jsApiTestPages/testList.html">FullTestList</a></li>
                         <li><Link to="/portalTest"><div>Portal</div></Link></li>
+                        <li><Link to="/nestedDivsTest"><div>Nested Divs</div></Link></li>
                     </ul>
                 </div>
             </div>
             <Routes>
                 <Route path="/" element={<HomePage />}></Route>
                 <Route path="/portalTest" element={<PortalTest />}></Route>
+                <Route path="/nestedDivsTest" element={<NestedDivsTest />}></Route>
             </Routes>
         </HashRouter>
     </React.StrictMode>
