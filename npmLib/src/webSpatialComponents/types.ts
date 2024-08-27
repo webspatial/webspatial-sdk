1ea738659726581b5a4d040c9cce327575b5a41c
trevorbaron <trevorbaron@bytedance.com>
2024-08-27 13:57:22 -0700

    avoid error when only specifying z postion for spatial div

--- npmLib/src/webSpatialComponents/types.ts
+++ npmLib/src/webSpatialComponents/types.ts
@@ -1,7 +1,7 @@
 export type vecType = { x: number, y: number, z: number }
 export type quatType = { x: number, y: number, z: number, w: number }
 export type spatialStyleDef = {
-    position: vecType,
+    position: Partial<vecType>,
     rotation: quatType,
     transparentEffect: boolean,
     glassEffect: boolean,
