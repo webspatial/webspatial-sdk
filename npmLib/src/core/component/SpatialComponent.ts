169eddf2612fc34e4c1aba2d487990087778afab
trevorbaron <trevorbaron@bytedance.com>
2024-09-03 13:31:27 -0700

    add getEntity api on component

--- npmLib/src/core/component/SpatialComponent.ts
+++ npmLib/src/core/component/SpatialComponent.ts
@@ -1,3 +1,15 @@
+import { WebSpatial, WebSpatialResource } from "../private/WebSpatial"
+import { SpatialEntity } from "../SpatialEntity"
 import { SpatialObject } from "../SpatialObject"
-export class SpatialComponent extends SpatialObject { 
+export class SpatialComponent extends SpatialObject {
+    async getEntity() {
+        let reqResp: any = await WebSpatial.updateResource(WebSpatial.getCurrentWebPanel(), { getEntityID: "" })
+        if (reqResp.data.parentID === "") {
+            return new Promise<SpatialEntity | null>((res, rej) => { res(null) })
+        } else {
+            var res = new WebSpatialResource()
+            res.id = reqResp.data.parentID
+            return new SpatialEntity(res)
+        }
+    }
 }
