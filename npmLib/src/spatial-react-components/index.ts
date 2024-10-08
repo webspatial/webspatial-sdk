dbf44beb0cec1890f1f981ad18c7d3875a5004cd
王任 wangren <wangren.walter@bytedance.com>
2024-10-08 19:21:13 +0800

    feat: support Internal Styles for web spatial;  make inline-style have higher priority than internal style

--- npmLib/src/spatial-react-components/index.ts
+++ npmLib/src/spatial-react-components/index.ts
@@ -4,6 +4,7 @@ export { SpatialPrimitive, withSpatial } from './SpatialPrimitive';
 export { type SpatialReactComponentProps, type SpatialReactComponentRef } from './SpatialReactComponent';
 
 export { monitorGlobalStyles } from './useSpatialStyle';
+export {injectWebSpatialCapability} from './globalInject'
 
 // for compatibility
 export { SpatialDiv } from './SpatialPrimitive';
