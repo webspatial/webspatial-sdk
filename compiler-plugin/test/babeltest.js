1147a2e93bea85e0ebcb0c548ad53593f03a5074
王任 wangren <wangren.walter@bytedance.com>
2024-10-11 10:17:29 +0800

    code refractor, remove compiler-plugin related code

--- compiler-plugin/test/babeltest.js
+++
@@ -1,8 +0,0 @@
-//# sourceURL=dynamicScript.js
-
-// import generate from "@babel/generator";
-const {spatialBabelJSXTransform} = require("../babel-plugin-jsx-spatial");
-
-const output = spatialBabelJSXTransform("test/input.tsx");
-
-console.log(output);
