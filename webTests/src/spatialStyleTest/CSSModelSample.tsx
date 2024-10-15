a96efaebc4063abd1462e47674fe0dc5cdfaa969
王任 wangren <wangren.walter@bytedance.com>
2024-10-15 11:45:10 +0800

    1. add a postcss plugin to do spatialstyle transform 2. add esbuild css modules plugin to support css module

--- webTests/src/spatialStyleTest/CSSModelSample.tsx
+++ webTests/src/spatialStyleTest/CSSModelSample.tsx
@@ -1,33 +1,4 @@
-// import {useState} from 'react';
-import styles from './styles.module.css';
-// import { SpatialDiv } from 'web-spatial';
+import styles from './styles.module.scss';
 
-export const CSSModelSample = () => <div className={styles.localName} > this is a CSSModule Sample </div>
-    
-    
-//     {
-    
 
-//     const [isPrimary, setIsPrimary] = useState(true)
-//     const onClick = () => {
-//         setIsPrimary(v=> !v)
-//     }
-
-//     const style = {
-//         // background: 'blue',
-//         // back: isPrimary ? 72: 46
-//     } 
-
-//     console.log('dbg in CSSModelSample')
-//     const back = isPrimary ? 72: 46
-
-//     const spatialStyle = {
-//         position: {z: back}
-//     }
-
-//     return (
-//         <SpatialDiv  spatialStyle={spatialStyle}  >
-//              <div className={isPrimary ? styles.localName: styles.localName2} onClick={onClick}>this is a CSSModule Sample</div>
-//         </SpatialDiv>
-//     )
-// }
+export const CSSModelSample = () => <div isspatial className={styles.localName} > this is a CSSModule Sample </div>
