7f58397723a7ddfacd41b966275c800530f0f27a
王任 wangren <wangren.walter@bytedance.com>
2024-10-31 20:30:01 +0800

    remove old SpatialReactComponent and rename SpatialReactComponentRefactor to SpatialReactComponent Directory

--- npmLib/src/spatial-react-components/index.ts
+++ npmLib/src/spatial-react-components/index.ts
@@ -1,10 +1,10 @@
 export { Model, type ModelProps, type ModelRef } from './Model';
 export { SpatialIFrame } from './SpatialIFrame';
 export { SpatialPrimitive, withSpatial } from './SpatialPrimitive';
-export { type SpatialReactComponentProps, type SpatialReactComponentRef } from './SpatialReactComponentRefactor';
+export { type SpatialReactComponentProps, type SpatialReactComponentRef } from './SpatialReactComponent';
 export {SpatialMonitor} from './SpatialMonitor';
 export {notifyUpdateStandInstanceLayout} from './notifyUpdateStandInstanceLayout'
 
 // for compatibility
 export { SpatialDiv } from './SpatialPrimitive';
-export { type SpatialReactComponentProps as SpatialDivProps, type SpatialReactComponentRef as SpatialDivRef } from './SpatialReactComponentRefactor';
+export { type SpatialReactComponentProps as SpatialDivProps, type SpatialReactComponentRef as SpatialDivRef } from './SpatialReactComponent';
