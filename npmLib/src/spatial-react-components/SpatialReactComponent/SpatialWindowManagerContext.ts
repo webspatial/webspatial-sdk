c45c7df0fc43b6e5efe97b6b12bbf26f894320da
王任 wangren <wangren.walter@bytedance.com>
2024-10-31 20:30:01 +0800

    remove old SpatialReactComponent and rename SpatialReactComponentRefactor to SpatialReactComponent Directory

--- npmLib/src/spatial-react-components/SpatialReactComponent/SpatialWindowManagerContext.ts
+++ npmLib/src/spatial-react-components/SpatialReactComponent/SpatialWindowManagerContext.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react";
 import { SpatialWindowManager } from "./SpatialWindowManager";
 
-export const SpatialWindowManagerContext = createContext(null as null | SpatialWindowManager);
+export const SpatialWindowManagerContext = createContext<SpatialWindowManager | undefined>(undefined);
