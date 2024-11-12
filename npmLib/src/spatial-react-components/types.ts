4fee81cb743dce4d115bcc9e1331a4403b80f3a8
王任 wangren <wangren.walter@bytedance.com>
2024-11-12 14:42:54 +0800

    support entity scale (without scale anchor)

--- npmLib/src/spatial-react-components/types.ts
+++ npmLib/src/spatial-react-components/types.ts
@@ -3,6 +3,7 @@ export type quatType = { x: number, y: number, z: number, w: number }
 export type spatialStyleDef = {
     position: Partial<vecType>,
     rotation: quatType,
+    scale: Partial<vecType>,
     transparentEffect: boolean,
     glassEffect: boolean,
     materialThickness: "none" | "thick" | "regular" | "thin",
