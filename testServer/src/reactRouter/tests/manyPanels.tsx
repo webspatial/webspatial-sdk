8bdf27ae25d137f275da04b835c39fece61d482b
trevorbaron <trevorbaron@bytedance.com>
2024-11-19 15:22:43 -0800

    server compiling without errors

--- testServer/src/reactRouter/tests/manyPanels.tsx
+++ testServer/src/reactRouter/tests/manyPanels.tsx
@@ -1,6 +1,6 @@
 import anime from "animejs"
 import { useEffect, useRef, useState } from "react"
-import { SpatialDiv } from "@xrsdk/runtime"
+import { SpatialDiv } from "@xrsdk/react"
 
 
 var redCol = "#cc111144"
