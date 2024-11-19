1fd4cfdd80d1ab1f90c771147778137df899119f
trevorbaron <trevorbaron@bytedance.com>
2024-11-19 13:36:56 -0800

    setup new folder structure and copy files to them

--- npmLib/src/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './core' 
-export * from './spatial-react-components'
-export * from './utils'
-
-// expose WebSpatialLogger for temp use, should be deleted later
-import { WebSpatial } from './core/private/WebSpatial'
-export const WebSpatialLogger = WebSpatial.logger
