462f3002f87b60903b2d012473b3d52f515b7352
trevorbaron <trevorbaron@bytedance.com>
2024-11-19 15:22:43 -0800

    server compiling without errors

--- testServer/src/reactRouter/tests/popup.tsx
+++ testServer/src/reactRouter/tests/popup.tsx
@@ -1,6 +1,6 @@
 import anime from "animejs";
 import { useEffect, useRef, useState } from "react"
-import { SpatialDiv } from "@xrsdk/runtime"
+import { SpatialDiv } from "@xrsdk/react"
 
 export function PopupTest() {
     const [popupEnabled, setPopupEnabled] = useState(true)
