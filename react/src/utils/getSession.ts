dcdead60ead698b412ab39583c9681f5529a1f76
王任 wangren <wangren.walter@bytedance.com>
2024-11-21 18:00:17 +0800

    add debug tool for easier debug

--- react/src/utils/getSession.ts
+++ react/src/utils/getSession.ts
@@ -1,36 +1,19 @@
-import { Spatial, SpatialSession } from "@xrsdk/runtime"
+import { Spatial, SpatialSession } from "@xrsdk/runtime";
 
 // Create the default Spatial session for the app
-let spatial = new Spatial()
-let _currentSession = null as SpatialSession | null
+let spatial = new Spatial();
+let _currentSession = null as SpatialSession | null;
 /** @hidden */
 export function getSession() {
-    if (!spatial.isSupported()) {
-        return null
-    }
-    if (_currentSession) {
-        return _currentSession
-    }
-    _currentSession = spatial.requestSession();
+  if (!spatial.isSupported()) {
+    return null;
+  }
+  if (_currentSession) {
+    return _currentSession;
+  }
+  _currentSession = spatial.requestSession();
 
-
-
-    return _currentSession
+  return _currentSession;
 }
 
-// (window as any).getSession = getSession;
-
-//     (window as any).getStat = async () => {
-//         const statsInfo = await getSession()!.getStats();
-
-//         return statsInfo
-//     };
-
-
-//     (window as any).inspectRootWindowGroup = async () => {
-//         const rootWindowGroupInfo = await getSession()!.inspectRootWindowGroup();
-//         console.log(rootWindowGroupInfo)
-//         return rootWindowGroupInfo
-//     };
-
-export { spatial }
+export { spatial };
