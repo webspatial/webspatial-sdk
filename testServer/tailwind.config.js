c2d834ce00f758e7425c9a360ace4206e3018a32
trevorbaron <trevorbaron@bytedance.com>
2024-11-22 11:02:23 -0800

    stop daisyUI from overwriting background colors which interfere with glass effect

--- testServer/tailwind.config.js
+++ testServer/tailwind.config.js
@@ -1,15 +1,14 @@
-import daisyui from "daisyui"
+import daisyui from 'daisyui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./index.tsx",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './index.tsx', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
+  daisyui: {
+    base: false,
+    themes: false, // Disable all themes
+  },
   plugins: [daisyui],
 }
-
