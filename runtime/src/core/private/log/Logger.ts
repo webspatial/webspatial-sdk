12a2d65113b1e6a3a3d7fee3374cc4d08c7ec41f
wangyang <wangyang.dev@bytedance.com>
2024-11-26 15:12:30 +0800

    chore: format all ts tsx code

--- runtime/src/core/private/log/Logger.ts
+++ runtime/src/core/private/log/Logger.ts
@@ -1,10 +1,10 @@
-import {LoggerLevel} from './LoggerLevel'
+import { LoggerLevel } from './LoggerLevel'
 
 export interface Logger {
-    setLevel(level: LoggerLevel): Promise<void>;
-    trace(...msg: any[]): Promise<void>;
-    debug(...msg: any[]): Promise<void>;
-    info(...msg: any[]): Promise<void>;
-    warn(...msg: any[]): Promise<void>;
-    error(...msg: any[]): Promise<void>;
+  setLevel(level: LoggerLevel): Promise<void>
+  trace(...msg: any[]): Promise<void>
+  debug(...msg: any[]): Promise<void>
+  info(...msg: any[]): Promise<void>
+  warn(...msg: any[]): Promise<void>
+  error(...msg: any[]): Promise<void>
 }
