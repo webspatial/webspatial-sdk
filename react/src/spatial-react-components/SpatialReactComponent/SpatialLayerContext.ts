68c4ac81827295be2a4817ea53bae13c1a1322ba
wangyang <wangyang.dev@bytedance.com>
2024-11-26 15:12:30 +0800

    chore: format all ts tsx code

--- react/src/spatial-react-components/SpatialReactComponent/SpatialLayerContext.ts
+++ react/src/spatial-react-components/SpatialReactComponent/SpatialLayerContext.ts
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext } from 'react'
 
 // SpatialLayerContext is used to mark the spatial layer of the spatial div, which is used to help portal instance find the correct layer standard instance div.
-export const SpatialLayerContext = createContext(0);
+export const SpatialLayerContext = createContext(0)
