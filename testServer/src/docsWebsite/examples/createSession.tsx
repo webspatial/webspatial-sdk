bef2e1c9889bf4a2ca455580e4da1dfba02aad15
trevorbaron <trevorbaron@bytedance.com>
2024-11-27 13:37:41 -0800

    add version api

--- testServer/src/docsWebsite/examples/createSession.tsx
+++ testServer/src/docsWebsite/examples/createSession.tsx
@@ -4,9 +4,17 @@ import { showSample } from './sampleLoader'
 
 function MySample(props: { session?: SpatialSession }) {
   var [supported, setSupported] = useState(false)
+  var [versionDisplay, setVersionDisplay] = useState('')
+
   useEffect(() => {
     // CODESAMPLE_START
     let spatial = new Spatial()
+    let versionLog =
+      'Client version: ' +
+      spatial.getClientVersion() +
+      ' Native Version: ' +
+      spatial.getNativeVersion()
+    setVersionDisplay(versionLog)
     if (spatial.isSupported()) {
       let session = spatial.requestSession()
       setSupported(true)
@@ -16,6 +24,7 @@ function MySample(props: { session?: SpatialSession }) {
   return (
     <div>
       SpatialSession is {supported ? '' : 'not'} supported in this browser
+      <h1>{versionDisplay}</h1>
     </div>
   )
 }
