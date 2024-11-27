56cb8dbdfbaca94363c9bbc2e759fc50b5e05711
trevorbaron <trevorbaron@bytedance.com>
2024-11-26 17:21:21 -0800

    add getting started page

--- testServer/src/docsWebsite/examples/home.tsx
+++ testServer/src/docsWebsite/examples/home.tsx
@@ -1,11 +1,21 @@
 import { SpatialSession } from '@xrsdk/runtime'
 import { useEffect, useState } from 'react'
 import { showSample } from './sampleLoader'
+import ReactMarkdown from 'react-markdown'
 
 function MySample(props: { session?: SpatialSession }) {
+  var [showMarkdown, setShowMarkdown] = useState('')
+  fetch('/src/docsWebsite/docs/gettingStarted.md')
+    .then(response => response.text())
+    .then(text => {
+      setShowMarkdown(text)
+    })
+
   return (
-    <div>
-      <a href="/">Homepage</a>
+    <div className="w-full">
+      <ReactMarkdown className="prose w-full max-w-none text-gray-200 prose-headings:text-gray-200 p-5">
+        {showMarkdown}
+      </ReactMarkdown>
     </div>
   )
 }
