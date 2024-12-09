959f005a2745421ab0a51679fdf3bdb5c0f34a56
wangren.walter <wangren.walter@bytedance.com>
2024-12-09 11:49:00 +0800

    support corner radius

--- testServer/src/reactRouter/tests/BackgroundMaterialTest.tsx
+++ testServer/src/reactRouter/tests/BackgroundMaterialTest.tsx
@@ -1,89 +1,166 @@
 import { SpatialDiv } from '@xrsdk/react'
+import { CornerRadius } from '@xrsdk/runtime/dist'
+import { useState } from 'react'
 
 export function BackgroundMaterialTest() {
   const z = 100
-  const cornerRadius = 20
+
+  const [bottomLeading, setBottomLeading] = useState(10)
+  const [bottomTrailing, setBottomTrailing] = useState(10)
+  const [topLeading, setTopLeading] = useState(10)
+  const [topTrailing, setTopTrailing] = useState(10)
+
+  const cornerRadius: CornerRadius = {
+    bottomLeading,
+    bottomTrailing,
+    topLeading,
+    topTrailing,
+  }
   return (
-    <div className="flex flex-row space-x-2 w-screen text-orange-500   text-center">
-      <SpatialDiv
-        spatialStyle={{
-          position: { z, x: 0 },
-          cornerRadius,
-          material: {
-            type: 'none',
-          },
-        }}
-        style={{
-          height: '100px',
-        }}
-        className="grow"
-      >
-        this is transparent material
-      </SpatialDiv>
+    <>
+      <div className="text-orange-200 mx-2.5 my-2.5">
+        <div className="">
+          borderRadius bottomLeading:
+          <input
+            type="range"
+            style={{ width: '50%' }}
+            min={0}
+            max="200"
+            value={bottomLeading}
+            onChange={e => {
+              setBottomLeading(Number(e.target.value))
+            }}
+            className="range"
+          />
+        </div>
 
-      <SpatialDiv
-        spatialStyle={{
-          position: { z, x: 0 },
-          cornerRadius,
-          material: {
-            type: 'default',
-          },
-        }}
-        style={{
-          height: '100px',
-        }}
-        className="grow bg-slate-50/50"
-      >
-        this is glass material
-      </SpatialDiv>
+        <div>
+          borderRadius bottomTrailing:
+          <input
+            type="range"
+            style={{ width: '50%' }}
+            min={0}
+            max="200"
+            value={bottomTrailing}
+            onChange={e => {
+              setBottomTrailing(Number(e.target.value))
+            }}
+            className="range"
+          />
+        </div>
 
-      <SpatialDiv
-        spatialStyle={{
-          position: { z, x: 0 },
-          cornerRadius,
-          material: {
-            type: 'thin',
-          },
-        }}
-        style={{
-          height: '100px',
-        }}
-        className="grow bg-slate-50/50"
-      >
-        this is thin material
-      </SpatialDiv>
+        <div>
+          borderRadius topLeading:
+          <input
+            type="range"
+            style={{ width: '50%' }}
+            min={0}
+            max="200"
+            value={topLeading}
+            onChange={e => {
+              setTopLeading(Number(e.target.value))
+            }}
+            className="range"
+          />
+        </div>
 
-      <SpatialDiv
-        spatialStyle={{
-          position: { z, x: 0 },
-          cornerRadius,
-          material: {
-            type: 'regular',
-          },
-        }}
-        style={{
-          height: '100px',
-        }}
-        className="grow bg-slate-50/50"
-      >
-        this is regular material
-      </SpatialDiv>
+        <div>
+          borderRadius topTrailing:
+          <input
+            type="range"
+            style={{ width: '50%' }}
+            min={0}
+            max="200"
+            value={topTrailing}
+            onChange={e => {
+              setTopTrailing(Number(e.target.value))
+            }}
+            className="range"
+          />
+        </div>
+      </div>
 
-      <SpatialDiv
-        spatialStyle={{
-          position: { z, x: 0 },
-          cornerRadius,
-          material: {
-            type: 'thick',
-          },
-        }}
-        style={{
-          height: '100px',
-        }}
-        className="grow bg-slate-50/50"
-      >
-        this is thick material
-      </SpatialDiv>
-    </div>
+      <div className="flex flex-row space-x-2 w-screen text-orange-500   text-center">
+        <SpatialDiv
+          spatialStyle={{
+            position: { z, x: 0 },
+            cornerRadius,
+            material: {
+              type: 'none',
+            },
+          }}
+          style={{
+            height: '100px',
+          }}
+          className="grow"
+        >
+          this is transparent material
+        </SpatialDiv>
+
+        <SpatialDiv
+          spatialStyle={{
+            position: { z, x: 0 },
+            cornerRadius,
+            material: {
+              type: 'default',
+            },
+          }}
+          style={{
+            height: '100px',
+          }}
+          className="grow bg-slate-50/50"
+        >
+          this is glass material
+        </SpatialDiv>
+
+        <SpatialDiv
+          spatialStyle={{
+            position: { z, x: 0 },
+            cornerRadius,
+            material: {
+              type: 'thin',
+            },
+          }}
+          style={{
+            height: '100px',
+          }}
+          className="grow bg-slate-50/50"
+        >
+          this is thin material
+        </SpatialDiv>
+
+        <SpatialDiv
+          spatialStyle={{
+            position: { z, x: 0 },
+            cornerRadius,
+            material: {
+              type: 'regular',
+            },
+          }}
+          style={{
+            height: '100px',
+          }}
+          className="grow bg-slate-50/50"
+        >
+          this is regular material
+        </SpatialDiv>
+
+        <SpatialDiv
+          spatialStyle={{
+            position: { z, x: 0 },
+            cornerRadius,
+            material: {
+              type: 'thick',
+            },
+          }}
+          style={{
+            height: '100px',
+          }}
+          className="grow bg-slate-50/50"
+        >
+          this is thick material
+        </SpatialDiv>
+      </div>
+    </>
   )
 }
