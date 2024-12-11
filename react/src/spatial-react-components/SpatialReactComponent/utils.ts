e9e789294ec388a7c5011e65cfc25eb40f75c5c4
王任 wangren <wangren.walter@bytedance.com>
2024-12-11 07:40:44 +0000

    Support css spatial style border corner
    * change '@xrsdk/runtime/dist' to '@xrsdk/runtime''
    
    
    * support spatialStyle cornerRadius
    
    
    See merge request: !118

--- react/src/spatial-react-components/SpatialReactComponent/utils.ts
+++ react/src/spatial-react-components/SpatialReactComponent/utils.ts
@@ -78,3 +78,39 @@ export function parseTransformOrigin(computedStyle: CSSStyleDeclaration) {
     z: 0.5,
   }
 }
+
+function parseBorderRadius(borderProperty: string, width: number) {
+  if (borderProperty === '') {
+    return 0
+  }
+  if (borderProperty.endsWith('%')) {
+    return (width * parseFloat(borderProperty)) / 100
+  }
+  return parseFloat(borderProperty)
+}
+
+export function parseCornerRadius(computedStyle: CSSStyleDeclaration) {
+  const width = parseFloat(computedStyle.getPropertyValue('width'))
+
+  const topLeftPropertyValue = computedStyle.getPropertyValue(
+    'border-top-left-radius',
+  )
+  const topRightPropertyValue = computedStyle.getPropertyValue(
+    'border-top-right-radius',
+  )
+  const bottomLeftPropertyValue = computedStyle.getPropertyValue(
+    'border-bottom-left-radius',
+  )
+  const bottomRightPropertyValue = computedStyle.getPropertyValue(
+    'border-bottom-right-radius',
+  )
+
+  const cornerRadius = {
+    topLeading: parseBorderRadius(topLeftPropertyValue, width),
+    bottomLeading: parseBorderRadius(bottomLeftPropertyValue, width),
+    topTrailing: parseBorderRadius(topRightPropertyValue, width),
+    bottomTrailing: parseBorderRadius(bottomRightPropertyValue, width),
+  }
+
+  return cornerRadius
+}
