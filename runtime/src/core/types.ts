5697e601b38e5cc2b78234da1c04b81e0af27e51
wangyang <wangyang.dev@bytedance.com>
2024-12-13 15:35:22 +0800

    add types

--- runtime/src/core/types.ts
+++ runtime/src/core/types.ts
@@ -1,1 +1,10 @@
 export type WindowStyle = 'Plain' | 'Volumetric'
+
+export interface WindowGroupOptions {
+  defaultSize?: {
+    width: number // Initial width of the window
+    height: number // Initial height of the window
+  }
+
+  resizability?: 'automatic' | 'contentSize' | 'contentMinSize'
+}
