d541664dfe21cf82ecafeaba74fbbf754464a22b
songjing <493570584@qq.com>
2025-01-20 20:55:32 +0800

    update

--- testServer/src/qaTestApp/depthPositionTest/depthPostion.tsx
+++ testServer/src/qaTestApp/depthPositionTest/depthPostion.tsx
@@ -1,16 +1,9 @@
 import React, { useRef, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { transform } from 'esbuild'
 
 function App() {
   const ref = useRef<HTMLDivElement>(null)
-  const ref1 = useRef<HTMLDivElement>(null)
-
   const [elementState, setElementState] = useState({
-    style: '',
-    className: '',
-  })
-  const [elementState1, setElementState1] = useState({
     style: '',
     className: '',
   })
@@ -28,39 +21,32 @@ function App() {
 
   useEffect(() => {
     updateElementState(ref)
-    updateElementState(ref1)
-  }, [ref, ref1])
+  }, [ref])
 
   const styleOne = {
-    '--xr-back': 10,
-    position: 'relative',
+    position: 'absolute',
     width: '200px',
     height: '78px',
-
+    top: '-5px',
     backgroundColor: 'red',
   }
 
-  const styleTwo = {
-    '--xr-back': 10,
-    backgroundColor: 'blue',
+  const containerStyle = {
     position: 'relative',
-    left: 0,
-    zIndex: 11,
+    width: '500px',
+    height: '300px',
+    backgroundColor: 'rgba(173, 216, 230, 0.2)',
+    border: '1px solid black',
+    margin: '50px auto',
   }
 
-  const styleThree = {
-    '--xr-back': 10,
+  const referenceStyle = {
+    position: 'absolute',
+    width: '100px',
+    height: '100px',
     backgroundColor: 'green',
-    position: 'relative',
-    left: 0,
-    zIndex: 3,
-  }
-  const resetStyles = () => {
-    if (!ref.current) return
-    ref.current.removeAttribute('style')
-    // ref.current.className =
-    //   'test-element w-32 h-32 bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg flex items-center justify-center text-white duration-300'
-    updateElementState(ref)
+    top: '50px',
+    left: '50px',
   }
 
   return (
@@ -75,7 +61,7 @@ function App() {
         </a>
         <a
           href="#"
-          onClick={() => history.go(-1)}
+          onClick={() => window.history.go(-1)}
           className="hover:text-blue-400 transition-colors"
         >
           返回上一级
@@ -83,32 +69,10 @@ function App() {
       </div>
 
       <div className="max-w-3xl mx-auto space-y-4">
-        <div className="bg-gray-800 p-4 rounded-lg min-h-[200px] flex items-center justify-center">
-          <div enable-xr style={styleOne}>
+        <div style={containerStyle}>
+          <div style={referenceStyle}>参考元素</div>
+          <div enable-xr style={styleOne} ref={ref}>
             one
-            <div enable-xr style={styleTwo}>
-              two
-              <div enable-xr style={styleThree}>
-                three
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-gray-800 p-4 rounded-lg">
-          <div className="grid grid-cols-2 gap-2">
-            {/*<button*/}
-            {/*  onClick={testPositionInlineStyles}*/}
-            {/*  className="p-2 bg-yellow-600 hover:bg-yellow-700 text-white rounded-lg transition-colors"*/}
-            {/*>*/}
-            {/*  Dimension Style Test*/}
-            {/*</button>*/}
-            <button
-              onClick={resetStyles}
-              className="p-2 bg-gray-600 hover:bg-gray-700 text-white rounded-lg transition-colors col-span-2"
-            >
-              Reset Styles
-            </button>
           </div>
         </div>
 
@@ -118,12 +82,6 @@ function App() {
             {ref.current
               ? `Style: ${elementState.style}
 Class Name: ${elementState.className}`
-              : 'Element Not Loaded'}
-          </pre>
-          <pre className="text-sm text-gray-300 whitespace-pre-wrap">
-            {ref1.current
-              ? `Style: ${elementState1.style}
-Class Name: ${elementState1.className}`
               : 'Element Not Loaded'}
           </pre>
         </div>
