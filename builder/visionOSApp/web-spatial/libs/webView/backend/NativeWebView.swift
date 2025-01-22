158d35b49fac16dc0d8ca4653c85d06b8ea96190
mzvast <mzvast@gmail.com>
2025-01-22 23:46:30 +0800

    initScene

--- builder/visionOSApp/web-spatial/libs/webView/backend/NativeWebView.swift
+++ builder/visionOSApp/web-spatial/libs/webView/backend/NativeWebView.swift
@@ -5,6 +5,7 @@
 //  Created by ByteDance on 5/9/24.
 //
 
+import Combine
 import Foundation
 import RealityKit
 import RealityKitContent
@@ -25,6 +26,14 @@ struct PreloadStyleSettings: Codable {
     var backgroundMaterial: BackgroundMaterial? = .None
 }
 
+struct WebviewEarlyStyle {
+    let webview: WKWebView
+    let style: PreloadStyleSettings
+}
+
+// event of forcestyle handler
+var webviewGetEarlyStyleData = PassthroughSubject<WebviewEarlyStyle, Never>()
+
 class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler, WKUIDelegate, UIScrollViewDelegate, WKURLSchemeHandler {
     let decoder = JSONDecoder()
     func webView(_ webView: WKWebView, start urlSchemeTask: any WKURLSchemeTask) {
@@ -41,7 +50,9 @@ class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler, WKUID
                     .components(separatedBy: "?").first
             }
             let styleToSet = try decoder.decode(PreloadStyleSettings.self, from: styleJsonString!.data(using: .utf8)!)
-            webViewRef?.didGetEarlyStyle(style: styleToSet)
+
+            webviewGetEarlyStyleData.send(WebviewEarlyStyle(webview: webView, style: styleToSet))
+
             // Respond with empty css file
             let response = ".ignoreThis{}".data(using: .utf8)
             let mimeType = "text/css"
@@ -116,10 +127,23 @@ class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler, WKUID
         for navigationAction: WKNavigationAction,
         windowFeatures: WKWindowFeatures
     ) -> WKWebView? {
+        // check url
+        if let url = navigationAction.request.url {
+            // TODO: pwa logic
+        }
+
         let wvNative = WebViewNative()
+
         _ = wvNative.createResources(configuration: configuration)
+
         webViewRef!.didSpawnWebView(wv: wvNative)
+
         return wvNative.webViewHolder.appleWebView
+    }
+
+    // handle close
+    func webViewDidClose(_ webView: WKWebView) {
+        webViewRef!.didCloseWebView()
     }
 
     // receive message from wkwebview
