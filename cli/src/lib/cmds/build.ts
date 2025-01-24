36646d42b84ceba12aa3e78236d1d05fd62ae315
JackHFK <344376484@qq.com>
2025-01-24 18:10:45 +0800

    basic cli: 70% - pwa: check & generate with manifest.json - resource: move web project & generate icon - xcode: bind the web-project folder to 「TARGETS - web-spatial - Copy Bundle Resource」 in the xcode project
    Change-Id: I76ec432cdb4c59eb6b3a02ac7f8daf559fb13690

---
+++ cli/src/lib/cmds/build.ts
@@ -0,0 +1,34 @@
+
+import {ParsedArgs} from "minimist";
+import {ConsoleLog, Log} from "../utils/Log";
+import {InitArgs, PWAGenerator} from "../pwa"
+import { ResourceManager } from "../resource";
+import { XcodeManager } from "../xcode";
+
+
+export async function start(args: ParsedArgs, log: Log = new ConsoleLog('help')): Promise<boolean>{
+    /**
+     * pwa步骤
+     * 1. 加载manifest.json
+     * 2. 检测manifest.json参数完整性
+     * 3. 检测start_url规则
+     * 4. 完善start_url、scope、display、deeplink配置
+     **/ 
+    let manifestInfo = await PWAGenerator.generator(args as unknown as InitArgs);
+    console.log(args["project"])
+    /**
+     * resource步骤
+     * 1. 若为本地项目，则
+     *  a. 检查并创建项目目录
+     *  b. 移动web工程
+     * 2. 生成icon图标
+     **/
+    if(!manifestInfo.fromNet){ // 如果为本地项目，则需要对项目进行移动
+        await ResourceManager.moveProjectFrom(args["project"])
+    }
+    const iconPath = await ResourceManager.generateIcon(manifestInfo)
+    console.log(iconPath)
+    
+    XcodeManager.parseProject();
+    return true
+}
