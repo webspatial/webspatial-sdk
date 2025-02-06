16e9a73b71b6599bfa250e4699edc5faece5b5fc
songjing <songjing.4935@bytedance.com>
2025-02-06 18:08:13 +0800

    update style

--- testServer/src/qaTestApp/SceneTest/scene.tsx
+++ testServer/src/qaTestApp/SceneTest/scene.tsx
@@ -1,18 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { SpatialDiv } from '@xrsdk/react/dist'
 import { Spatial } from '@xrsdk/runtime'
 import { initScene } from '@xrsdk/react'
 
-// const defaultConfig = {
-//   defaultSize: {
-//     width: 900,
-//     height: 900,
-//   },
-//     resizability:"automatic"
-// }
 const btnCls =
-  'p-2 border border-gray-700 hover:text-white bg-gray-700 hover:bg-blue-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2'
+  'select-none px-4 py-1 text-s font-semibold rounded-full border border-gray-700 hover:text-white bg-gray-700 hover:bg-gray-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2'
 const spatial = new Spatial()
 const spatialSupported = spatial.isSupported()
 
@@ -23,157 +15,272 @@ if (spatialSupported) {
     cornerRadius: 50,
   })
 }
-
+const extUrl = 'https://www.google.com/'
+const extUrl2 = 'https://developer.mozilla.org/zh-CN/'
 function App() {
-  const [windowRefs, setWindowRefs] = useState({
-    winARef: useRef<any>(null),
-    winBRef: useRef<any>(null),
-    winCRef: useRef<any>(null),
-    winDRef: useRef<any>(null),
-    winERef: useRef<any>(null),
-    winFRef: useRef<any>(null),
-  })
+  const [logs, setLogs] = useState('')
 
-  const handleOpenWindow = (ref: React.MutableRefObject<any>, url: string) => {
-    ref.current = window.open(url)
-  }
-  // const handleOpenWindow = async (ref: React.MutableRefObject<any>, url: string, sceneName: string) => {
-  //   await initScene(sceneName, defaultConfig => defaultConfig)
-  //   ref.current = window.open(url, sceneName)
-  // }
-  const handleCloseWindow = (windowRef: React.MutableRefObject<any>) => {
-    try {
-      if (!windowRef.current) {
-        console.log('no window')
-        return
-      }
-      if (windowRef.current?.closed) {
-        console.log('is already closed')
-      } else {
-        windowRef.current?.close?.()
-        console.log('close success')
-      }
-    } catch (error: any) {
-      console.log(error.message)
+  useEffect(() => {
+    //@ts-ignore
+    log('windowID:', window._webSpatialID)
+    window.onerror = (error: any) => {
+      log('error:', error.message)
     }
+
+    return () => {
+      window.onerror = null
+    }
+  }, [])
+
+  function startlog(str: string) {
+    setLogs(str)
   }
+
+  function log(...args: any[]) {
+    setLogs(pre => {
+      let ans = pre + '\n'
+      for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] === 'object') {
+          ans += JSON.stringify(args[i])
+        } else {
+          ans += args[i]
+        }
+      }
+      return ans
+    })
+  }
+
+  const winARef = useRef<any>(null)
+  const winBRef = useRef<any>(null)
+  const winCRef = useRef<any>(null)
 
   return (
-    <div className="min-h-screen bg-gray-900 p-44">
-      <h1 style={{ textAlign: 'center', fontSize: '36px' }}>
-        <SpatialDiv
-          spatialStyle={{
-            position: { z: 100 }, // z方向凸起50
-          }}
-          className="text-6xl font-bold text-white p-8 rounded-xl"
-        >
-          Window Group
-        </SpatialDiv>
-      </h1>
-      {/* Navigation Bar */}
-      <div className="flex text-white text-lg bg-black bg-opacity-25 p-4 gap-5 mb-4">
-        <a href="/" className="hover:text-blue-400 transition-colors">
-          返回主页
-        </a>
-        <a
-          href="#"
-          onClick={() => history.go(-1)}
-          className="hover:text-blue-400 transition-colors"
-        >
-          返回上一级
-        </a>
-      </div>
-      <div className="bg-gray-800 p-4 rounded-lg min-h-[200px] ">
-        {/*<div >*/}
-        {/* 按钮组A */}
-        <div className="flex items-center">
-          <button
-            className={btnCls}
-            onClick={() =>
-              handleOpenWindow(windowRefs.winARef, 'http://localhost:5173/')
+    <div className="pl-5 pt-2">
+      <h1 className="text-2xl text-black">openscene</h1>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open no name')
+          winARef.current = window.open(
+            'http://localhost:5173/src/scene/hook.html',
+            // 'http://localhost:5173/src/scene/xrapp.html',
+          )
+        }}
+      >
+        open hook
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open no name')
+          winARef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            // 'http://localhost:5173/src/scene/xrapp.html',
+          )
+        }}
+      >
+        open no name
+      </button>
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          initScene('sa', () => ({
+            defaultSize: {
+              width: 900,
+              height: 900,
+            },
+          }))
+          winARef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sa',
+          )
+          // winARef.current = window.open('', 'sa')
+        }}
+      >
+        window.open with initScene
+      </button>
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          winARef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sa',
+            'width=800,height=600',
+          )
+          // winARef.current = window.open('', 'sa')
+        }}
+      >
+        open sa
+      </button>
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          window.open('webspatial://createWindowContext')
+        }}
+      >
+        open createWindowContext
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open Google')
+          winARef.current = window.open(extUrl, 'sa')
+        }}
+      >
+        open sa google
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open MDN')
+          winARef.current = window.open(extUrl2, 'sa')
+        }}
+      >
+        open sa MDN
+      </button>
+
+      <a className={btnCls} href="https://www.google.com" target="sa">
+        open sa google a tag
+      </a>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          winBRef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sb',
+          )
+        }}
+      >
+        open sb xrapp
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          winBRef.current = window.open(extUrl, 'sb')
+        }}
+      >
+        open sb google
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          winCRef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sc',
+          )
+        }}
+      >
+        open sc
+      </button>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('open')
+          winARef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sa',
+          )
+          winBRef.current = window.open(
+            'http://localhost:5173/src/scene/xrapp.html',
+            'sb',
+          )
+        }}
+      >
+        open sa+sb
+      </button>
+
+      <h1 className="text-2xl text-black">close scene by local</h1>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('close')
+          try {
+            if (!winARef.current) {
+              log('no window')
+              return
             }
-          >
-            open winA
-          </button>
-          <div className="w-4"></div>
-          <button
-            className={btnCls}
-            onClick={() => handleCloseWindow(windowRefs.winARef)}
-          >
-            close winA
-          </button>
-        </div>
-        {/* 按钮组B */}
-        <div className="flex items-center mt-4">
-          <button
-            className={btnCls}
-            onClick={() => {
-              initScene('sa', () => ({
-                defaultSize: {
-                  width: 300,
-                  height: 300,
-                },
-              }))
-              handleOpenWindow(
-                windowRefs.winBRef,
-                'http://localhost:5173/src/qaTestApp/domapiTest/domapi1.html',
-              )
-            }}
-          >
-            open winB
-          </button>
-          <div className="w-4"></div>
-          <button
-            className={btnCls}
-            onClick={() => handleCloseWindow(windowRefs.winBRef)}
-          >
-            close winB
-          </button>
-        </div>
-        {/* 按钮组C */}
-        <div className="flex items-center mt-4">
-          <button
-            className={btnCls}
-            onClick={() =>
-              handleOpenWindow(
-                windowRefs.winCRef,
-                'http://localhost:5173/src/qaTestApp/domapiTest/domapi1.html',
-              )
+            if (winARef.current?.closed) {
+              log('is already closed')
+            } else {
+              winARef.current?.close?.()
+              log('close success')
             }
-          >
-            open winC
-          </button>
-          <div className="w-4"></div>
-          <button
-            className={btnCls}
-            onClick={() => handleCloseWindow(windowRefs.winCRef)}
-          >
-            close winC
-          </button>
-        </div>
-        {/* 按钮组D */}
-        <div className="flex items-center mt-4">
-          <button
-            className={btnCls}
-            onClick={() =>
-              handleOpenWindow(windowRefs.winDRef, 'http://google.com')
+          } catch (error: any) {
+            log(error.message)
+          }
+        }}
+      >
+        close sa
+      </button>
+      <button
+        className={btnCls}
+        onClick={async () => {
+          startlog('close')
+
+          try {
+            if (!winBRef.current) {
+              log('no window')
+              return
             }
-          >
-            open google
-          </button>
-          <div className="w-4"></div>
-          <button
-            className={btnCls}
-            onClick={() => handleCloseWindow(windowRefs.winDRef)}
-          >
-            close google
-          </button>
-        </div>
+            if (winBRef.current?.closed) {
+              log('is already closed')
+            } else {
+              winBRef.current?.close?.()
+              log('close success')
+            }
+          } catch (error: any) {
+            log(error.message)
+          }
+        }}
+      >
+        close sb
+      </button>
+
+      <h1 className="text-2xl text-black">cross test</h1>
+
+      <button
+        className={btnCls}
+        onClick={async () => {
+          //@ts-ignore
+          log('windowID:', window._webSpatialID)
+        }}
+      >
+        get window._webSpatialID
+      </button>
+      <div>
+        <div>console</div>
+        <p style={{ fontSize: '46px' }}>{logs}</p>
       </div>
     </div>
   )
 }
 
-const root = document.createElement('div')
+// Initialize react
+var root = document.createElement('div')
 document.body.appendChild(root)
-ReactDOM.createRoot(root).render(<App />)
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+document.documentElement.style.backgroundColor = 'transparent'
+document.body.style.backgroundColor = 'transparent'
+// Force page height to 100% to get centering to work
+document.documentElement.style.height = '100%'
+document.body.style.height = '100%'
+root.style.height = '100%'
