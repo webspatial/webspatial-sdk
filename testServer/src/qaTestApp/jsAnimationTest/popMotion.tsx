82270c29ca3dfb4d30e521c9dd92d00769dc0584
kuhat <1043744621@qq.com>
2025-02-07 14:38:37 +0800

    add nested tests for js animation.

--- testServer/src/qaTestApp/jsAnimationTest/popMotion.tsx
+++ testServer/src/qaTestApp/jsAnimationTest/popMotion.tsx
@@ -3,6 +3,7 @@ import { animate } from 'popmotion'
 
 export function PopmotionTest() {
   const ref = useRef<HTMLElement>(null)
+  const refChild = useRef<HTMLDivElement>(null)
 
   const onChangeBack = () => {
     animate({
@@ -44,6 +45,46 @@ export function PopmotionTest() {
     }
   }
 
+  const onChangeBackChild = () => {
+    animate({
+      from: 0,
+      to: 100,
+      onUpdate: latest => {
+        if (refChild.current) refChild.current.style['--xr-back'] = latest
+      },
+    })
+  }
+
+  const onChangeOpacityChild = () => {
+    animate({
+      from: 1,
+      to: 0.5,
+      onUpdate: latest => {
+        if (refChild.current) refChild.current.style.opacity = latest
+      },
+    })
+  }
+
+  const onChangeRotationChild = () => {
+    animate({
+      from: 360,
+      to: 0,
+      onUpdate: latest => {
+        if (refChild.current)
+          refChild.current.style.transform =
+            'translate3d(0,0,0) rotateY(' + latest + 'deg)'
+      },
+    })
+  }
+
+  const onResetChild = () => {
+    if (refChild.current) {
+      refChild.current.style.opacity = 1
+      refChild.current.style['--xr-back'] = 0
+      refChild.current.style.transform = 'none'
+    }
+  }
+
   return (
     <div className="max-w-3xl mx-auto space-y-4">
       <div className="text-white">
@@ -52,23 +93,47 @@ export function PopmotionTest() {
       <div
         enable-xr
         ref={ref}
-        className="test-element w-64 h-32 rounded-lg bg-white bg-opacity-10 flex items-center justify-center text-white"
+        className="test-element w-64 h-32 rounded-lg bg-blue-50 bg-opacity-10 flex items-center justify-center text-white"
       >
         this is spatial div
+        <div
+          enable-xr
+          ref={refChild}
+          className="test-element w-64 h-32 rounded-lg bg-white bg-opacity-10 flex items-center justify-center text-white"
+        >
+          This is child spatial div
+        </div>
       </div>
-      <button className="btn btn-primary" onClick={onChangeBack}>
-        start animate back
-      </button>
-      <button className="btn btn-primary" onClick={onChangeOpacity}>
-        start animate opacity
-      </button>
-      <button className="btn btn-primary" onClick={onChangeRotation}>
-        start animate transform
-      </button>
 
-      <button className="btn btn-primary" onClick={onReset}>
-        reset
-      </button>
+      <div className="grid grid-cols-4 gap-4">
+        <button className="btn btn-primary" onClick={onChangeBack}>
+          start animate back
+        </button>
+        <button className="btn btn-primary" onClick={onChangeOpacity}>
+          start animate opacity
+        </button>
+        <button className="btn btn-primary" onClick={onChangeRotation}>
+          start animate transform
+        </button>
+        <button className="btn btn-primary" onClick={onReset}>
+          reset
+        </button>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4">
+        <button className="btn btn-primary" onClick={onChangeBackChild}>
+          start animate back
+        </button>
+        <button className="btn btn-primary" onClick={onChangeOpacityChild}>
+          start animate opacity
+        </button>
+        <button className="btn btn-primary" onClick={onChangeRotationChild}>
+          start animate transform
+        </button>
+        <button className="btn btn-primary" onClick={onResetChild}>
+          reset
+        </button>
+      </div>
     </div>
   )
 }
