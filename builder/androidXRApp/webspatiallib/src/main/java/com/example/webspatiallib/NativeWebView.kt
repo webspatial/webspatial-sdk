ed5e5c864ba8bf397b6d8dbdae0dc956d6864f49
trevorbaron <trevorbaron@bytedance.com>
2025-02-12 13:34:10 -0800

    add javascript native api

--- builder/androidXRApp/webspatiallib/src/main/java/com/example/webspatiallib/NativeWebView.kt
+++ builder/androidXRApp/webspatiallib/src/main/java/com/example/webspatiallib/NativeWebView.kt
@@ -2,12 +2,19 @@ package com.example.webspatiallib
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.view.View
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
 
 class NativeWebView {
     val webView:WebView;
-    @SuppressLint("[ByDesign3.3]AvoidContentOrFileExecuteJS")
+    @SuppressLint("[ByDesign3.3]AvoidContentOrFileExecuteJS",
+        "[ByDesign5.1]UsingAddJavaScriptInterface"
+    )
     constructor(context: Context){
         webView = WebView(context)
         webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
@@ -20,6 +27,17 @@ class NativeWebView {
         webView.settings.setSupportMultipleWindows(true)
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         webView.settings.defaultTextEncodingName = "utf-8"
+        webView.addJavascriptInterface(object {
+            @JavascriptInterface
+            fun getNativeVersion(): String {
+               return "0.0.1";
+            }
+
+            @JavascriptInterface
+            fun nativeMessage(message: String) {
+            }
+        }, "WebSpatailEnabled")
+        webView.setWebViewClient(WebViewClient()) // Allow navigation to navigate within webview (instead of open chrome)
     }
 
     fun navigateToURL(url: String) {
