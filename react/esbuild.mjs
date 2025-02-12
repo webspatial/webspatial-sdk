49c36e29d7b2f1f085d593c3b44c10eb2ce36576
mzvast <mzvast@gmail.com>
2025-02-12 12:21:58 +0800

    refactor: better treeshaking

--- react/esbuild.mjs
+++ react/esbuild.mjs
@@ -1,21 +1,96 @@
 import * as esbuild from 'esbuild'
 import dtsPlugin from 'esbuild-plugin-d.ts'
 import glob from 'tiny-glob'
+//@ts-ignore
+import path from 'path'
+//@ts-ignore
+import fs from 'fs'
 
 const entryPoints = await glob('./src/**/*.tsx')
 const entryPointsTs = await glob('./src/**/*.ts')
 const allEntryPoints = entryPoints.concat(entryPointsTs)
+function clearDist(dir) {
+  const distPath = path.resolve(dir)
+  if (fs.existsSync(distPath)) {
+    fs.rmSync(distPath, { recursive: true, force: true })
+    console.log(`Cleared ${distPath}`)
+  }
+}
+// for developer to use, we should ship 2 kinds of output
+// 1. ESM unbundled, used with compiling tools (this not treeshaking well for unused import)
+// 2. ESM bundled, used by <script> tag
+
+// to target platforms we provide:
+// 1. default. AVP impl
+// 2. web.
+
+// so we have 2*2 matrix as follow:
+// - ESM + default unbundled ❌
+// - ESM + web unbundled ❌(this not treeshaking well for unused import)
+// - ESM + default bundled ✅
+// - ESM + web bundled ✅
+
+// unbundled ESM cannot remove unused import
 
 const targets = [
+  // {
+  //   name: 'ESM + default unbundled',
+  //   entryPoints: allEntryPoints,
+  // dir: 'dist/default',
+  //   outdir: 'dist/default',
+  //   define: { __WEB__: 'false' },
+  //   // outfile: 'dist/default/index.js',
+  //   tsconfig: 'tsconfig.default.json',
+  //   format: 'esm',
+  //   bundle: false,
+  // },
+  // {
+  //   name: 'ESM + web unbundled',
+  //   entryPoints: allEntryPoints,
+  // dir: 'dist/web',
+  //   outdir: 'dist/web',
+  //   define: { __WEB__: 'true' },
+  //   // outfile: 'dist/web/index.js',
+  //   tsconfig: 'tsconfig.web.json',
+  //   format: 'esm',
+  //   bundle: false,
+  // },
   {
-    name: 'h5',
-    dir: 'dist/h5',
-    define: { __WEB__: 'true' },
-  },
-  {
-    name: 'default',
+    name: 'ESM + default bundled',
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/default/index.js',
     dir: 'dist/default',
     define: { __WEB__: 'false' },
+    tsconfig: 'tsconfig.default.json',
+    format: 'esm',
+    bundle: true,
+    external: [
+      'react',
+      'react-dom',
+      'three',
+      '@xrsdk/runtime',
+      'lodash.isequal',
+      '@google/model-viewer',
+    ],
+  },
+  {
+    name: 'ESM + web bundled',
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/web/index.js',
+    dir: 'dist/web',
+    define: { __WEB__: 'true' },
+    tsconfig: 'tsconfig.web.json',
+    format: 'esm',
+    bundle: true,
+    external: [
+      'react',
+      'react-dom',
+      'three',
+      '@xrsdk/runtime',
+      'lodash.isequal',
+      '@google/model-viewer',
+    ],
+    alias: { '@xrsdk/runtime': './src/noRuntime.ts' }, // replace the reference to runtime with empty module
   },
 ]
 
@@ -23,18 +98,28 @@ async function run() {
   const contexts = []
 
   for (const target of targets) {
+    clearDist(target.dir)
+
     const ctx = await esbuild.context({
-      entryPoints: allEntryPoints,
-      outdir: target.dir,
-      bundle: false,
-      format: 'esm',
+      entryPoints: target.entryPoints, //['./src/index.ts'],
+      outdir: target.outdir,
+      outfile: target.outfile,
+      bundle: target.bundle,
+      //@ts-ignore
+      format: target.format,
       keepNames: true,
-      minify: true, // fixme: enable this to remove unreachable code inside function
+      // minify: true, // fixme: enable this to remove unreachable code inside function
       treeShaking: true,
       sourcemap: true,
       jsx: 'automatic',
-      plugins: [dtsPlugin()],
+      plugins: [
+        dtsPlugin({
+          tsconfig: target.tsconfig,
+        }),
+      ],
       define: target.define,
+      alias: target.alias,
+      external: target.external,
     })
 
     contexts.push(ctx)
