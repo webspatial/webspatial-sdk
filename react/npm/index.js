fd76db2c45f51fd7f68b32ef216b436344458046
mzvast <mzvast@gmail.com>
2025-02-25 17:27:07 +0800

    chore

--- react/npm/index.js
+++ react/npm/index.js
@@ -1,8 +1,7 @@
 'use strict'
 
-console.log('process.env.NODE_ENV=', process.env.NODE_ENV)
 if (process.env.XR_ENV === 'web') {
-  module.exports = require('./cjs/index.web.js')
+  module.exports = require('../dist/cjs/web/index.js')
 } else {
-  module.exports = require('./cjs/index.default.js')
+  module.exports = require('../dist/cjs/default/index.js')
 }
