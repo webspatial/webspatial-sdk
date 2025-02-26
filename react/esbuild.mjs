b5be88232fab2c52d3b0d4922a236a7ef1865918
mzvast <mzvast@gmail.com>
2025-02-26 10:57:58 +0800

    clean commented out code& unused

--- react/esbuild.mjs
+++ react/esbuild.mjs
@@ -18,22 +18,6 @@ function clearDist(dir) {
 
 // for dynamic choose version we should ship 2 kinds of cjs output and switch by env variable
 
-// for developer to use, we should ship 2 kinds of output
-// 1. ESM unbundled, used with compiling tools (this not treeshaking well for unused import)
-// 2. ESM bundled, used by <script> tag
-
-// to target platforms we provide:
-// 1. default. AVP impl
-// 2. web.
-
-// so we have 2*2 matrix as follow:
-// - ESM + default unbundled ❌
-// - ESM + web unbundled ❌(this not treeshaking well for unused import)
-// - ESM + default bundled ✅
-// - ESM + web bundled ✅
-
-// unbundled ESM cannot remove unused import
-
 const targets = [
   {
     name: 'CJS + default bundled',
@@ -72,43 +56,6 @@ const targets = [
     ],
     alias: { '@xrsdk/runtime': './src/noRuntime.ts' }, // replace the reference to runtime with empty module
   },
-  // {
-  //   name: 'ESM + default bundled',
-  //   entryPoints: ['./src/index.ts'],
-  //   outfile: 'dist/default/index.js',
-  //   dir: 'dist/default',
-  //   define: { __WEB__: 'false' },
-  //   tsconfig: 'tsconfig.default.json',
-  //   format: 'esm',
-  //   bundle: true,
-  //   external: [
-  //     'react',
-  //     'react-dom',
-  //     'three',
-  //     '@xrsdk/runtime',
-  //     'lodash.isequal',
-  //     '@google/model-viewer',
-  //   ],
-  // },
-  // {
-  //   name: 'ESM + web bundled',
-  //   entryPoints: ['./src/index.ts'],
-  //   outfile: 'dist/web/index.js',
-  //   dir: 'dist/web',
-  //   define: { __WEB__: 'true' },
-  //   tsconfig: 'tsconfig.web.json',
-  //   format: 'esm',
-  //   bundle: true,
-  //   external: [
-  //     'react',
-  //     'react-dom',
-  //     'three',
-  //     '@xrsdk/runtime',
-  //     'lodash.isequal',
-  //     '@google/model-viewer',
-  //   ],
-  //   alias: { '@xrsdk/runtime': './src/noRuntime.ts' }, // replace the reference to runtime with empty module
-  // },
 ]
 
 async function run() {
@@ -142,9 +89,6 @@ async function run() {
     contexts.push(ctx)
   }
 
-  // mkDistDir()
-  // copyNpmToDist()
-
   // Check if --watch flag is provided in the command line arguments
   const watchMode = process.argv.includes('--watch')
 
@@ -161,24 +105,6 @@ async function run() {
 
     process.exit(0)
   }
-}
-
-function mkDistDir() {
-  const distPath = path.resolve('dist')
-  if (!fs.existsSync(distPath)) {
-    fs.mkdirSync(distPath, { recursive: true })
-    console.log(`Created dist directory at ${distPath}`)
-  }
-}
-
-function copyNpmToDist() {
-  exec('cp -rf npm/* dist/', (err, stdout, stderr) => {
-    if (err) {
-      console.error('Error copying npm folder:', err)
-      return
-    }
-    console.log('Copied npm folder to dist:', stdout)
-  })
 }
 
 run()
