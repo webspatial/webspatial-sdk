822a9e3b4b17d70aaa249df6df130c1c51d5c244
JackHFK <344376484@qq.com>
2025-02-28 10:42:31 +0800

    bind mainScene data from manifest to manifest.swift

--- cli/src/lib/xcode/xcodeproject.ts
+++ cli/src/lib/xcode/xcodeproject.ts
@@ -286,7 +286,7 @@ export default class XcodeProject {
     )
     manifestSwift = manifestSwift.replace(
       'SceneResizability',
-      manifest.mainScene.resizability,
+      '.' + manifest.mainScene.resizability,
     )
     fs.writeFileSync(manifestSwiftPath, manifestSwift, 'utf-8')
   }
