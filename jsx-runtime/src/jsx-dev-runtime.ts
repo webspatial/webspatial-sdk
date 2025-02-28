d7c465be999c4135d45d43be65a888cc5129e1fa
mzvast <mzvast@gmail.com>
2025-02-28 17:15:57 +0800

    chore: seperate shared code and prevent reference from each other. es spatialPolyfill

--- jsx-runtime/src/jsx-dev-runtime.ts
+++ jsx-runtime/src/jsx-dev-runtime.ts
@@ -1,22 +1,6 @@
-'use strict'
-
-import { jsxDEV as _jsxDEV, JSXSource } from 'react/jsx-dev-runtime'
 import { spatialPolyfill } from './spatialPolyfill'
-import { replaceToSpatialPrimitiveType } from './jsx-runtime'
-
-export function jsxDEV(
-  type: React.ElementType,
-  props: unknown,
-  key: React.Key,
-  isStatic: boolean,
-  source?: JSXSource,
-  self?: unknown,
-) {
-  type = replaceToSpatialPrimitiveType(type, props)
-  return _jsxDEV(type, props, key, isStatic, source, self)
-}
 
 spatialPolyfill()
 
-export { jsx } from './jsx-runtime'
 export { Fragment } from 'react/jsx-runtime'
+export { jsxDEV, jsx } from './jsx-shared'
