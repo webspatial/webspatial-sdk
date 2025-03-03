d7b2bc4f0012fa0b221cbd8dccb120ef6fef4bb8
mzvast <mzvast@gmail.com>
2025-03-03 11:39:50 +0800

    rename

--- examples/react-vite/src/App.tsx
+++ examples/react-vite/src/App.tsx
@@ -1,27 +1,54 @@
+import * as React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { SpatialDiv } from '@xrsdk/react'
+import { SpatialDiv } from '@webspatial/react-sdk'
+import { Spatial } from '@webspatial/core-sdk'
 
+const spatial = new Spatial()
+const spatialSupported = spatial.isSupported()
+
+if (spatialSupported) {
+  var session = new Spatial().requestSession()
+  session!.getCurrentWindowComponent().setStyle({
+    material: { type: 'translucent' },
+    cornerRadius: 50,
+  })
+}
+document.documentElement.style.backgroundColor = 'transparent'
 function App() {
   const [count, setCount] = useState(0)
-
+  const spatialStyle = {
+    color: 'red',
+    backgrondColor: 'black',
+    position: { x: 0, y: 0, z: 1.000001 },
+    transparentEffect: false,
+    glassEffect: false,
+    // materialThickness: "none"
+  }
   return (
     <>
-      <SpatialDiv spatialStyle={{ position: { z: 50 }, cornerRadius: 10 }}>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </SpatialDiv>
+      </div>
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
+
+        <SpatialDiv spatialStyle={spatialStyle}>
+          {' '}
+          this is spatial div{' '}
+        </SpatialDiv>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- examples/react-vite/vite.config.ts
+++ examples/react-vite/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import xrsdkPlugin from '@xrsdk/react/plugin-vite.js'
+import xrsdkPlugin from '@webspatial/react-sdk/plugin-vite.js'
 
 // https://vite.dev/config/
 
