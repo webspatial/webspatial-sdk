ad2f0ccdd74d54058ce9a2c0a43b1ae3a0ea4699
songjing <songjing.4935@bytedance.com>
2025-03-04 15:46:20 +0800

    update qatest.tsx and add model3dTest

--- testServer/src/qaTestApp/qatest.tsx
+++ testServer/src/qaTestApp/qatest.tsx
@@ -96,6 +96,13 @@ function App() {
           <h2 className="text-xl font-bold mb-2">Animation Test</h2>
           <p className="text-sm opacity-80">3rd party JS Animation Tests</p>
         </a>
+        <a
+          href="/src/qaTestApp/model3dTest/model3d.html"
+          className="p-6 bg-blue-500 bg-opacity-25 rounded-xl text-white hover:bg-opacity-40 transition-all"
+        >
+          <h2 className="text-xl font-bold mb-2">Model3D Test</h2>
+          <p className="text-sm opacity-80">Model Support Tests</p>
+        </a>
       </div>
     </div>
   )
