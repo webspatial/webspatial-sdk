bed671f18af04cd94276f02240fa6981275bb7dc
JackHFK <344376484@qq.com>
2025-03-05 14:19:02 +0800

    Create Xcode project template in cli Rename store command to publish Improve the help command

---
+++ cli/template/visionOSApp/web-spatial/libs/uiKitDelegate/Window.swift
@@ -0,0 +1,34 @@
+//
+//  Window.swift
+//  web-spatial
+//
+//  Created by ByteDance on 7/3/24.
+//
+
+import Foundation
+import SwiftUI
+
+// Access window (https://stackoverflow.com/questions/60359808/how-to-access-own-window-within-swiftui-view/60359809#60359809)
+class SceneDelegate: NSObject, ObservableObject, UIWindowSceneDelegate {
+    var window: UIWindow? // << contract of `UIWindowSceneDelegate`
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = windowScene.keyWindow // << store !!!
+    }
+
+    // do memory cleanup after scene removed, otherwise windowContainer cannot destroy content after being dismissed
+    func sceneDidDisconnect(_ scene: UIScene) {
+        window = nil
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        if connectingSceneSession.role == .windowApplication {
+            configuration.delegateClass = SceneDelegate.self
+        }
+        return configuration
+    }
+}
---
+++ cli/template/visionOSApp/web-spatial/libs/webView/UpdateSystem.swift
@@ -0,0 +1,33 @@
+//
+//  UpdateSystem.swift
+//  web-spatial
+//
+//  Created by ByteDance on 5/21/24.
+//
+
+import Foundation
+import RealityKit
+
+struct UpdateWebViewComponent: Component {
+    var webView: SpatialWindowComponent?
+    init() {}
+}
+
+class UpdateWebViewSystem: System {
+    static let query = EntityQuery(where: .has(UpdateWebViewComponent.self))
+    required init(scene: RealityKit.Scene) {
+        // Perform required initialization or setup.
+    }
+
+    var pos = 0.0
+    func update(context: SceneUpdateContext) {
+        for entity in context.entities(matching: Self.query, updatingSystemWhen: .rendering) {
+            pos += context.deltaTime
+
+            var x = Transform()
+            x.translation.x = Float(sin(pos)) * 0.3
+            x.translation.z = 0.2
+            entity.move(to: x, relativeTo: nil)
+        }
+    }
+}
