097055f6bbac8735a90e050ac54bde3740b85f7f
mzvast <mzvast@gmail.com>
2025-03-07 22:38:18 +0800

    change plugin vite

--- examples/react-vite/vite.config.ts
+++ examples/react-vite/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import xrsdkPlugin from '@webspatial/react-sdk/plugin-vite.js'
+// import xrsdkPlugin from '../../react-plugin-vite/dist'
+import xrsdkPlugin from '@webspatial/react-plugin-vite'
+// import xrsdkPlugin from '@webspatial/react-sdk/plugin-vite.js'
 
 // https://vite.dev/config/
 
