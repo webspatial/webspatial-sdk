12c9750d59041b786c9abb2a83c312744c3940ab
JackHFK <344376484@qq.com>
2025-03-11 23:04:12 +0800

    Update navigation UI and expand animation.

--- cli/template/visionOSApp/web-spatial/views/ui/NavView.swift
+++ cli/template/visionOSApp/web-spatial/views/ui/NavView.swift
@@ -10,119 +10,116 @@ import WebKit
 
 struct NavView: View {
     @State var swc: SpatialWindowComponent?
-    @State var showUrl: Bool = false
+    @State var showUrl: Bool = true
     @State private var showCopyTip = false
-    @State private var nowHover = false
     @State private var navWidth: CGFloat = 0
+    @State private var navHeight: CGFloat = 0
+    @State private var texWidth: CGFloat = 0
+    @State private var firstGetSize: Bool = true
 
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
-            HStack(spacing: 5) {
-                Text(pwaManager.name).padding(.trailing, 10)
-                if pwaManager.display == .minimal {
-                    Button(action: {
-                        swc?.goBack()
-                    }, label: {
-                        Image(systemName: "arrow.left")
-                    })
-                    .disabled(!(swc?.canGoBack ?? false))
-                    Button(action: {
-                        swc?.goForward()
-                    }, label: {
-                        Image(systemName: "arrow.right")
-                    })
-                    .disabled(!(swc?.canGoBack ?? false))
-                    Button(action: {
-                        swc?.reload()
-                    }, label: {
-                        Image(systemName: "arrow.clockwise")
-                    })
-                    Button(
-                        action: {
-                            swc?
-                                .navigateToURL(
-                                    url: URL(string: pwaManager.start_url)!
-                                )
-                        },
-                        label: {
-                            Image(systemName: "house.fill")
+            ZStack {}.frame(width: navWidth, height: navHeight)
+            VStack(alignment: .trailing, spacing: 5) {
+                if showUrl {
+                    Text(pwaManager.name).padding(10)
+                    if pwaManager.display == .minimal {
+                        HStack(spacing: 5) {
+                            Button(action: {
+                                swc?.goBack()
+                            }, label: {
+                                Image(systemName: "arrow.left")
+                            })
+                            .disabled(!(swc?.canGoBack ?? false))
+                            Button(action: {
+                                swc?.goForward()
+                            }, label: {
+                                Image(systemName: "arrow.right")
+                            })
+                            .disabled(!(swc?.canGoBack ?? false))
+                            Button(action: {
+                                swc?.reload()
+                            }, label: {
+                                Image(systemName: "arrow.clockwise")
+                            })
+                            Button(
+                                action: {
+                                    swc?
+                                        .navigateToURL(
+                                            url: URL(string: pwaManager.start_url)!
+                                        )
+                                },
+                                label: {
+                                    Image(systemName: "house.fill")
+                                }
+                            )
                         }
-                    )
+                    }
                 }
-                Button(action: {
-                    showUrl.toggle()
-                }, label: {
-                    Image(systemName: "info.circle")
-                })
-            }.overlay(GeometryReader { geo -> AnyView in
+                HStack(spacing: 5) {
+                    if showUrl {
+                        Text(
+                            swc?.getURL()?.absoluteString ?? "http://localhost:5173/"
+                        )
+                        .padding()
+                        .lineLimit(1)
+                        .overlay(GeometryReader { geo -> AnyView in
+                            DispatchQueue.main.async {
+                                if geo.size.width > 0 {
+                                    texWidth = .minimum(300, geo.size.width)
+                                }
+                            }
+                            return AnyView(EmptyView())
+                        })
+                        .frame(width: texWidth == 0 ? .infinity : texWidth)
+                        Button(action: {
+                            UIPasteboard.general.string = swc?.getURL()?.absoluteString ?? ""
+                            showCopyTip = true
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                showCopyTip = false
+                            }
+                        }, label: {
+                            Text("copy")
+                        })
+                    }
+
+                    Button(action: {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            showUrl.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: showUrl ? "xmark" : "info")
+                    })
+                }
+            }
+            .padding()
+            .glassBackgroundEffect(in: RoundedRectangle(cornerRadius: 15))
+            .popover(isPresented: $showCopyTip) {
+                Text("copied！")
+                    .padding()
+                    .cornerRadius(10)
+            }
+            .overlay(GeometryReader { geo -> AnyView in
                 DispatchQueue.main.async {
-                    navWidth = geo.size.width
-                    print(navWidth)
+                    if firstGetSize {
+                        navWidth = geo.size.width
+                        navHeight = geo.size.height
+                        firstGetSize.toggle()
+                        showUrl.toggle()
+                        print(navWidth)
+                    }
                 }
                 return AnyView(EmptyView())
             })
-            .hoverEffect { effect, isHover, body in
-                effect.clipShape(getMaskPath(rect: CGRect(x: isHover || showUrl ? 0 : body.size.width - 65, y: 0, width: body.size.width, height: body.size.height)))
-//                effect.clipShape(.rect.size(width: isHover || showUrl ? 0 : body.size.width - 65, height: body.size.height))
-            }
-            .gesture(SpatialTapGesture().onEnded { evt in
-                let rangeX = navWidth - evt.location.x
-                if rangeX < 60 {
-                    showUrl.toggle()
-                } else if rangeX >= 74, rangeX <= 140 {
-                    swc?
-                        .navigateToURL(
-                            url: URL(string: pwaManager.start_url)!
-                        )
-                } else if rangeX >= 150, rangeX <= 210 {
-                    swc?.reload()
-                } else if rangeX >= 221, rangeX <= 281 {
-                    swc?.goForward()
-                } else if rangeX >= 294, rangeX <= 355 {
-                    swc?.goBack()
-                }
-            })
-            HStack(spacing: 5) {
-                Text(
-                    swc?.getURL()?.absoluteString ?? ""
-                )
-                .frame(maxWidth: navWidth)
-                .padding()
-                Button(action: {
-                    UIPasteboard.general.string = swc?.getURL()?.absoluteString ?? ""
-                    showCopyTip = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        showCopyTip = false
-                    }
-                }, label: {
-                    Text("copy")
-                })
-
-                Button(action: {
-                    showUrl = false
-                }, label: {
-                    Text("X")
-                })
-            }.glassBackgroundEffect(in: .rect).cornerRadius(15).offset(y: 0).opacity(showUrl ? 1 : 0).animation(.easeOut, value: 0.2)
+            .opacity(firstGetSize ? 0 : 1)
         }
-        .zIndex(10) // closer to user
-        .popover(isPresented: $showCopyTip) {
-            Text("copied！")
-                .padding()
-                .cornerRadius(10)
-//                .background(.ultraThinMaterial)
-        }
-    }
-
-    func getMaskPath(rect: CGRect) -> Path {
-        var path = Path()
-        path.addRoundedRect(in: rect, cornerSize: CGSize(width: 15, height: 15))
-        return path
+        .frame(width: firstGetSize ? .infinity : navWidth, height: firstGetSize ? .infinity : navHeight)
+        .offset(z: 30)
     }
 }
 
-// struct NavView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NavView()
-//    }
-// }
+struct NavView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavView()
+    }
+}
