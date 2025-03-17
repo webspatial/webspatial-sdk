add03e8ddd48d5b9263b61734962a4bd3cdefdd7
JackHFK <344376484@qq.com>
2025-03-17 17:55:11 +0800

    Create a new npm package as @webspatial/platform-avp by builder/visionOS

---
+++ builder/visionOS/web-spatial/libs/SpatialComponent.swift
@@ -0,0 +1,31 @@
+//
+//  SpatialComponent.swift
+//  web-spatial
+//
+//  Created by ByteDance on 9/10/24.
+//
+
+import Foundation
+
+class SpatialComponent: SpatialObject {
+    weak var entity: SpatialEntity? = nil
+
+    func onAddToEntity() {}
+
+    override func onDestroy() {
+        entity = nil
+    }
+
+    override func inspect() -> [String: Any] {
+        var inspectInfo: [String: Any] = [
+            "entity": entity?.id,
+            "type": String(describing: type(of: self)),
+        ]
+
+        let baseInspectInfo = super.inspect()
+        for (key, value) in baseInspectInfo {
+            inspectInfo[key] = value
+        }
+        return inspectInfo
+    }
+}
---
+++ builder/visionOS/web-spatial/libs/SpatialModel3DComponent.swift
@@ -0,0 +1,51 @@
+import Foundation
+import SwiftUI
+
+@Observable
+class SpatialModel3DComponent: SpatialComponent {
+    var resolutionX: Double = 0
+    var resolutionY: Double = 0
+    var modelURL: String = ""
+    var opacity: Double = 1.0
+    var rotationAnchor: UnitPoint3D = .center
+    var scrollWithParent = true
+    var contentMode: ContentMode = .fit
+    var resizable: Bool = true
+    var aspectRatio: Double? = nil
+
+    var enableTapEvent = false
+    var enableDoubleTapEvent = false
+    var enableLongPressEvent = false
+    var enableDragEvent = false
+
+    // SpatialModel3DView should not have dependency on SpatialWindowComponent.
+    // It just need some JSB communication channel. This need to be refactor in future
+    weak var wv: SpatialWindowComponent?
+
+    public func setURL(_ url: String) {
+        modelURL = url
+    }
+
+    override func inspect() -> [String: Any] {
+        var inspectInfo: [String: Any] = [
+            "scrollWithParent": scrollWithParent,
+            "resolutionX": resolutionX,
+            "resolutionY": resolutionY,
+            "modelURL": modelURL,
+            "opacity": opacity,
+            "resizable": resizable,
+            "aspectRatio": aspectRatio != nil ? String(describing: aspectRatio!) : "nil",
+            "contentMode": contentMode == .fill ? "fill" : "fit",
+            "enableTapEvent": enableTapEvent,
+            "enableDoubleTapEvent": enableDoubleTapEvent,
+            "enableLongPressEvent": enableLongPressEvent,
+            "enableDragEvent": enableDragEvent,
+        ]
+
+        let baseInspectInfo = super.inspect()
+        for (key, value) in baseInspectInfo {
+            inspectInfo[key] = value
+        }
+        return inspectInfo
+    }
+}
