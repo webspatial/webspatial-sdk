80652db501000e47805a2823c025697a986d4f5f
walter.wang <wangren.walter@bytedance.com>
2025-03-19 18:08:57 +0800

    find the nearest scroll enabled webview to scroll

--- testServer/src/nestedscroll/index.tsx
+++ testServer/src/nestedscroll/index.tsx
@@ -12,19 +12,23 @@ function App() {
 
   const styleHead = {
     height: '200px',
+    fontSize: '50px',
+    color: 'white',
     background: 'linear-gradient(to bottom, yellow, green)',
   }
 
   const styleLongContent = {
     height: '200vh',
+    fontSize: '50px',
+    color: 'white',
     background: 'linear-gradient(to bottom, blue, green)',
     '--xr-back': 121,
   }
 
   return (
-    <div style={styleContainer}>
+    <div style={styleContainer} debugName="container">
       <div style={styleHead}> head </div>
-      <div style={styleLongContent} enable-xr>
+      <div style={styleLongContent} enable-xr debugName="longcontent">
         long content
       </div>
     </div>
