d4c1b6a07118af74d8c13800bd66f1e2fa3beaf7
mzvast <mzvast@gmail.com>
2025-03-27 20:56:05 +0800

    update logic about outDir and base

--- react-plugin-vite/src/shared.ts
+++ react-plugin-vite/src/shared.ts
@@ -1,7 +1,39 @@
 const AVP = 'avp'
-function getEnv() {
+function getEnv(): ModeKind {
   const env = process.env.XR_ENV || ''
-  return env
+  return env as ModeKind
 }
 
+type ModeKind = 'avp' | undefined
+
 export { getEnv, AVP }
+
+export function getFinalBase(
+  userBase: string | undefined,
+  mode: ModeKind,
+  outputDir?: string,
+) {
+  if (mode === 'avp') {
+    if (userBase !== undefined) {
+      return userBase
+    } else {
+      return outputDir
+    }
+  }
+
+  // web version
+  return userBase
+}
+
+export function getFinalOutdir(
+  userOutDir: string | undefined = 'dist',
+  mode: ModeKind,
+  pluginOutputDir?: string,
+) {
+  if (mode === 'avp') {
+    return `${userOutDir}/${pluginOutputDir}`
+  }
+
+  // web version
+  return userOutDir
+}
