653c2af0867eaf47a3c8f0305661e7ed4dd99848
mzvast <mzvast@gmail.com>
2025-04-10 12:04:16 +0800

    chore add initScene.web

--- react/src/jsx/jsx-dev-runtime.ts
+++ react/src/jsx/jsx-dev-runtime.ts
@@ -7,5 +7,3 @@ export { jsxDEV, jsx } from './jsx-shared'
 
 export type { WebSpatialJSX as JSX } from './jsx-namespace'
 export * from './xr-css-extension'
-
-console.log('jsx avp')
--- react/src/jsx/jsx-dev-runtime.web.ts
+++ react/src/jsx/jsx-dev-runtime.web.ts
@@ -1,4 +1,2 @@
 export { Fragment } from 'react/jsx-runtime'
 export { jsxDEV, jsxDEV as jsx } from 'react/jsx-dev-runtime'
-
-console.log('jsx web')
