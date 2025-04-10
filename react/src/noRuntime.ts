f55a966e2b4895e1e102befe34baa4ffa6958483
mzvast <mzvast@gmail.com>
2025-04-10 11:39:11 +0800

    update empty module to satisfy class signature

--- react/src/noRuntime.ts
+++ react/src/noRuntime.ts
@@ -3,4 +3,33 @@ export default {}
 
 export const SpatialHelper = {}
 
-export const Spatial = {}
+export class Spatial {
+  /**
+   * Requests a session object from the browser
+   * @returns The session or null if not availible in the current browser
+   * [TODO] discuss implications of this not being async
+   */
+  requestSession() {
+    return null
+  }
+  /**
+   * @returns true if web spatial is supported by this webpage
+   */
+  isSupported() {
+    return false
+  }
+  /**
+   * Gets the native version, format is "x.x.x"
+   * @returns native version string
+   */
+  getNativeVersion() {
+    return null
+  }
+  /**
+   * Gets the client version, format is "x.x.x"
+   * @returns client version string
+   */
+  getClientVersion() {
+    return null
+  }
+}
