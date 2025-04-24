487045aec20f27a9247fc7b7a24e3527acc7b0da
trevorbaron <trevorbaron@bytedance.com>
2025-04-24 11:28:41 -0700

    revert https://github.com/webspatial/webspatial-sdk/pull/628 (acaa2df)

--- builder/visionOS/web-spatial/libs/Utils/CommandManager.swift
+++ builder/visionOS/web-spatial/libs/Utils/CommandManager.swift
@@ -492,14 +492,14 @@ class CommandManager {
             }
 
             if let backgroundMaterial: BackgroundMaterial = data.update?.style?.backgroundMaterial {
-                if spatialWindowComponent.isLoading {
-                    spatialWindowComponent.loadingStyles.backgroundMaterial = backgroundMaterial
+                if target.isLoading {
+                    target.loadingStyles.backgroundMaterial = backgroundMaterial
                 }
                 spatialWindowComponent.backgroundMaterial = backgroundMaterial
             }
             if let cornerRadius: CornerRadius = data.update?.style?.cornerRadius {
-                if spatialWindowComponent.isLoading {
-                    spatialWindowComponent.loadingStyles.cornerRadius = cornerRadius
+                if target.isLoading {
+                    target.loadingStyles.cornerRadius = cornerRadius
                 }
                 spatialWindowComponent.cornerRadius = cornerRadius
             }
