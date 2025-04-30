039dc2e4d7dcac377c13357af05c9de077b7df29
trevorbaron <trevorbaron@bytedance.com>
2025-04-30 13:12:31 -0700

    updateBackgroundMaterialFromJSBridge

--- builder/androidXRApp/webspatiallib/src/main/java/com/example/webspatiallib/SpatialWindowComponent.kt
+++ builder/androidXRApp/webspatiallib/src/main/java/com/example/webspatiallib/SpatialWindowComponent.kt
@@ -1,12 +1,18 @@
 package com.example.webspatiallib
 
 import android.content.Context
+import android.util.MutableFloat
+import android.util.MutableInt
+import androidx.compose.runtime.getValue
 import java.lang.ref.WeakReference
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class SpatialWindowComponent(context: Context) : SpatialComponent() {
     val nativeWebView = NativeWebView(context)
 
-    var backgroundStyle = "none"
+    var backgroundStyle by mutableStateOf("none")
 
     init {
         nativeWebView.windowComponent = WeakReference(this)
