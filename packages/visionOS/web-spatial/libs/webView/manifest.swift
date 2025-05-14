b7102ea2bea934eaca743a090ba1feaa3ca92ad1
mzvast <mzvast@gmail.com>
2025-05-14 11:39:11 +0800

    support resizeRange

--- packages/visionOS/web-spatial/libs/webView/manifest.swift
+++ packages/visionOS/web-spatial/libs/webView/manifest.swift
@@ -21,7 +21,8 @@ struct PWAManager: Codable {
             width: 1280,
             height: 720
         ),
-        resizability: "automatic"
+        resizability: "automatic",
+        resizeRange: nil
     )
     var useMainScene: Bool = true
     private var version: String = "PACKAGE_VERSION"
