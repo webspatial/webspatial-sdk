823bcafe12a4b20f5bf9e336f0ed49aecfb385e5
walter.wang <wangren.walter@bytedance.com>
2025-05-20 15:08:37 +0800

    update Licenses and Attributions in CONTRIBUTING.md

--- tools/scripts/generate-attributions.js
+++ tools/scripts/generate-attributions.js
@@ -23,37 +23,61 @@ var getLicense = cmd => {
 
 var main = async () => {
   var runtimeDeps = await getLicense(
-    'cd runtime && npx license-checker --json && cd ..',
+    'cd packages/core && npx license-checker --json && cd ../..',
   )
   var reactDeps = await getLicense(
-    'cd react && npx license-checker --json && cd ..',
+    'cd packages/react && npx license-checker --json && cd ../..',
   )
+
+  var cliDeps = await getLicense(
+    'cd packages/cli && npx license-checker --json && cd ../..',
+  )
+
   var testServerDeps = await getLicense(
-    'cd testServer && npx license-checker --json && cd ..',
+    'cd apps/test-server && npx license-checker --json && cd ../..',
   )
+
+  var citestDeps = await getLicense(
+    'cd tests/ci-test && npx license-checker --json && cd ../..',
+  )
+
   var rootDeps = await getLicense('npx license-checker --json')
 
   var dependencies = {
     ...runtimeDeps,
     ...reactDeps,
+    ...cliDeps,
     ...testServerDeps,
+    ...citestDeps,
     ...rootDeps,
   }
   let attributionText =
     '## Licenses and Attributions\n\nThis project uses the following third-party libraries:\n\n'
 
   for (const [packageName, details] of Object.entries(dependencies)) {
+    if (packageName.startsWith('@webspatial')) {
+      continue
+    }
+    if (packageName.indexOf('webspatial-sdk') >= 0) {
+      continue
+    }
     attributionText += `- [${packageName}](${details.repository || 'No repository link'}) - Licensed under the ${details.licenses}\n`
   }
 
-  // Append or write to README.md
-  const readmePath = './README.md'
-  if (fs.existsSync(readmePath)) {
-    fs.appendFileSync(readmePath, `\n\n${attributionText}`)
-    console.log('Attributions added to README.md')
+  // Replace ## Licenses and Attributions with the new text
+  const contributingFile = './CONTRIBUTING.md'
+  if (fs.existsSync(contributingFile)) {
+    const originalContent = fs.readFileSync(contributingFile, 'utf-8')
+
+    const idx = originalContent.indexOf('## Licenses and Attributions')
+    const prevContent = originalContent.substring(0, idx)
+
+    const newContent = prevContent.concat(attributionText)
+
+    fs.writeFileSync(contributingFile, newContent)
+    console.log('finished')
   } else {
-    fs.writeFileSync(readmePath, attributionText)
-    console.log('README.md created with attributions.')
+    console.log('attributionText \n', attributionText)
   }
 }
 main()
