864a8d0a0a231b8a394d46c8b5f33f0e2e703bea
wangyang <wangyang.dev@bytedance.com>
2025-08-01 11:59:17 +0800

    add UpdateSceneConfigCommand

--- packages/core/src/refactor/SpatialScene.ts
+++ packages/core/src/refactor/SpatialScene.ts
@@ -1,11 +1,26 @@
 import { SpatialSceneProperties } from './types'
 import {
   AddSpatializedElementToSpatialScene,
+  UpdateSceneConfig,
   UpdateSpatialSceneProperties,
 } from './JSBCommand'
 
 import { SpatializedElement } from './SpatializedElement'
 import { SpatialObject } from './SpatialObject'
+
+export interface SpatialSceneOptions {
+  defaultSize?: {
+    width: number // Initial width of the window
+    height: number // Initial height of the window
+  }
+
+  resizability?: {
+    minWidth?: number
+    minHeight?: number
+    maxWidth?: number
+    maxHeight?: number
+  }
+}
 
 let instance: SpatialScene
 
@@ -23,5 +38,9 @@ export class SpatialScene extends SpatialObject {
 
   async addSpatializedElement(element: SpatializedElement) {
     return new AddSpatializedElementToSpatialScene(element).execute()
+  }
+
+  async updateSceneConfig(config: SpatialSceneOptions) {
+    return new UpdateSceneConfig(config).execute()
   }
 }
