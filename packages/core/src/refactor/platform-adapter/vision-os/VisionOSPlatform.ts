578f006cd53d08c51fcb16f113454769b819039d
wangyang <wangyang.dev@bytedance.com>
2025-08-01 11:33:45 +0800

    add callWebSpatialProtocolSync

--- packages/core/src/refactor/platform-adapter/vision-os/VisionOSPlatform.ts
+++ packages/core/src/refactor/platform-adapter/vision-os/VisionOSPlatform.ts
@@ -38,14 +38,36 @@ export class VisionOSPlatform implements PlatformAbility {
     command: string,
     query?: string,
   ): Promise<CommandResult> {
+    const { spatialId: id, windowProxy } = this.openWindow(command, query)
+    return Promise.resolve(
+      CommandResultSuccess({ windowProxy: windowProxy, id }),
+    )
+  }
+
+  callWebSpatialProtocolSync(
+    command: string,
+    query?: string,
+    resultCallback?: (result: CommandResult) => void,
+  ): WindowProxy | null {
+    const { spatialId: id, windowProxy } = this.openWindow(command, query)
+
+    if (resultCallback) {
+      setTimeout(() => {
+        const result = CommandResultSuccess({ windowProxy: windowProxy, id })
+        resultCallback(result)
+      }, 0)
+    }
+
+    return windowProxy
+  }
+
+  private openWindow(command: string, query?: String) {
     const windowProxy = window.open(`webspatial://${command}?${query || ''}`)
     const ua = windowProxy?.navigator.userAgent
     const spatialId = ua?.match(
       /\b([0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12})\b/gi,
     )?.[0]
 
-    return Promise.resolve(
-      CommandResultSuccess({ windowProxy: windowProxy, id: spatialId }),
-    )
+    return { spatialId, windowProxy }
   }
 }
