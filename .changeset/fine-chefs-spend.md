8e3cf1c2a79a188905fad0662756007113287d2a
mzvast <mzvast@gmail.com>
2025-08-27 16:33:04 +0800

    Revert "fix: backport fix for a link in spatialDiv"

--- .changeset/fine-chefs-spend.md
+++
@@ -1,5 +0,0 @@
----
-'@webspatial/react-sdk': patch
----
-
-fix a link issue in spatialDiv
--- packages/react/src/XRApp.ts
+++ packages/react/src/XRApp.ts
@@ -29,7 +29,6 @@ export class XRApp {
       if (target && target !== '_self') {
         event.preventDefault()
         window.open(url, target)
-        return true
       }
     }
   }
