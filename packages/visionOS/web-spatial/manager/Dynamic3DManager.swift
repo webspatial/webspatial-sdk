2db477c30cb9bad5c3da690d55785f6a9f99ea61
hongfukang <hongfukang@bytedance.com>
2025-09-05 17:18:49 +0800

    add framework file

---
+++ packages/visionOS/web-spatial/manager/Dynamic3DManager.swift
@@ -0,0 +1,93 @@
+//
+//  Dymaic3DManager.swift
+//  web-spatial
+//
+//  Created by ByteDance on 2025/9/5.
+//
+
+import RealityKit
+
+class Dynamic3DManager{
+    private var entities:[String:Entity] = [:]
+    private var components:[String:Component] = [:]
+    private var materials:[String:Material] = [:]
+    private var geometries:[String:MeshResource] = [:]
+    private var textures:[String:TextureResource] = [:]
+    private var modelAssets:[String:Entity] = [:]
+    
+    func createEntity(){
+        
+    }
+    
+    func createComponent(){
+        
+    }
+    
+    func createGeometry(){
+        
+    }
+    
+    func createMaterial(){
+        
+    }
+    
+    func createTexture(){
+        
+    }
+    
+    func createModelAsset(){
+        
+    }
+    
+    func getEntity(){
+        
+    }
+    
+    func getComponent(){
+        
+    }
+    
+    func getGeometry(){
+        
+    }
+    
+    func getMaterial(){
+        
+    }
+    
+    func getTexture(){
+        
+    }
+    
+    func getModelAsset(){
+        
+    }
+    
+    func destroy(){
+        
+    }
+    
+    private func destroyEntity(){
+        
+    }
+    
+    private func destroyComponent(){
+        
+    }
+    
+    private func destroyGeometry(){
+        
+    }
+    
+    private func destroyMaterial(){
+        
+    }
+    
+    private func destroyTexture(){
+        
+    }
+    
+    private func destroysModelAsset(){
+        
+    }
+}
--- packages/visionOS/web-spatial/view/SpatializedDynamic3DView.swift
+++ packages/visionOS/web-spatial/view/SpatializedDynamic3DView.swift
@@ -3,30 +3,77 @@ import SwiftUI
 
 struct SpatializedDynamic3DView: View {
     @Environment(SpatializedElement.self) var spatializedElement: SpatializedElement
+    @State private var isDrag = false
+    @State private var isRotate = false
+    @State private var isScale = false
     
-    private func createCube() -> Entity {
-
-        let entity = Entity()
-        entity.components.set([
-            ModelComponent(
-                mesh: .generateBox(size: 0.1, cornerRadius: 0.01),
-                materials: [SimpleMaterial()]),
-            InputTargetComponent(allowedInputTypes: .indirect),
-            CollisionComponent(shapes: [
-                ShapeResource.generateBox(size: [0.1, 0.1, 0.1])
-            ])
-        ])
-        
-        return entity
+    private var spatializedDynamic3DElement: SpatializedDynamic3DElement {
+        return spatializedElement as! SpatializedDynamic3DElement
+    }
+    
+    var spatialTapEvent: some Gesture{
+        SpatialTapGesture(count: 1).targetedToAnyEntity()
+            .onEnded{ value in
+                
+        }
+    }
+    
+    var rotate3dEvent: some Gesture{
+        RotateGesture3D().targetedToAnyEntity().onChanged{ value in
+            print(value.gestureValue)
+            if(isRotate == false){
+                print("start rotate")
+            }
+            else{
+                print("rotating")
+            }
+            isRotate = true
+        }.onEnded{ value in
+            print(value.rotation)
+            print("rotate end")
+            isRotate = false
+        }
+    }
+    
+    var magnifyEvent: some Gesture{
+        MagnifyGesture().targetedToAnyEntity().onChanged{ value in
+            print(value)
+            if(isScale == false){
+                print("start scale")
+            }
+            else{
+                print("scaling")
+            }
+            isScale = true
+        }.onEnded{ value in
+            print("scale end")
+            isScale = false
+        }
+    }
+    
+    var dragEvent: some Gesture{
+        DragGesture().targetedToAnyEntity().onChanged{ value in
+            if(isDrag == false){
+                print("start drag")
+            }
+            else{
+                print("dragging")
+            }
+            isDrag = true
+            
+        }.onEnded{ value in
+            print("drag end")
+            isDrag = false
+        }
     }
     
     var body: some View {
-        RealityView { content in
-            let cube = createCube()
-            cube.position.x = 0
-            cube.position.y = 0
-            cube.position.z = 0
-            content.add(cube)
-        }
+        RealityView(make: { content in
+            
+        })
+        .simultaneousGesture(spatialTapEvent)
+        .simultaneousGesture(rotate3dEvent)
+        .simultaneousGesture(dragEvent)
+        .simultaneousGesture(magnifyEvent)
     }
 }
