1be0e00bd880a29ca4b9bd268c375e7dda026151
wangyang <wangyang.dev@bytedance.com>
2025-09-16 21:24:06 +0800

    add pureReality as backup

---
+++ packages/react/src/reality/components/PureReality.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { RealityContext, RealityContextValue } from '../context'
+
+import { getSession } from '../../utils/getSession'
+import { ResourceRegistry } from '../utils'
+type Props = {
+  children?: React.ReactNode
+}
+
+export const PureReality: React.FC<Props> = ({ children }) => {
+  const ctxRef = useRef<RealityContextValue>(null)
+  const [isReady, setIsReady] = useState(false)
+  useEffect(() => {
+    let cancelled = false
+
+    const init = async () => {
+      const resourceRegistry = new ResourceRegistry()
+      const session = await getSession()
+      if (!session) {
+        console.error('getSession failed')
+        return
+      }
+      const reality = await session.createSpatializedDynamic3DElement()
+      if (cancelled) {
+        reality.destroy()
+        return
+      }
+      await session.getSpatialScene().addSpatializedElement(reality)
+      ctxRef.current = { session, reality, resourceRegistry }
+      setIsReady(true)
+    }
+    init()
+    return () => {
+      cancelled = true
+      ctxRef.current?.resourceRegistry.destroy()
+      ctxRef.current?.reality.destroy()
+    }
+  }, [])
+
+  if (!isReady) return null
+
+  return (
+    <RealityContext.Provider value={ctxRef.current}>
+      {children}
+    </RealityContext.Provider>
+  )
+}
