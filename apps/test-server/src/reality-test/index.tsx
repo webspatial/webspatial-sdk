8665e38d33ab77b38ec25e1ec89de0f87a2ca367
hongfukang <hongfukang@bytedance.com>
2025-09-18 11:20:05 +0800

    update reality-test

--- apps/test-server/src/reality-test/index.tsx
+++ apps/test-server/src/reality-test/index.tsx
@@ -1,122 +1,267 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-
 import {
-  enableDebugTool
+  BoxEntity,
+  Entity,
+  EntityRef,
+  getSession,
+  ModelAsset,
+  ModelEntity,
+  Reality,
+  SceneGraph,
+  SpatialTapEvent,
+  UnlitMaterial,
 } from '@webspatial/react-sdk'
-import { useEffect, useRef } from 'react'
+import { Particle } from './particle'
 
-enableDebugTool()
+export interface EntityInfo {
+  id?: string | number
+  side: 'left' | 'right'
+  position: {
+    x: number
+    y: number
+    z: number
+  },
+  materials: string[]
+  scale?: {
+    x: number
+    y: number
+    z: number
+  },
+  cornerRadius?: number
+}
 
 function App() {
-  const entityRef = useRef<any>(null)
-  const modelEntityRef = useRef<any>(null)
-  const angleRef = useRef<number>(45)
-  const matrixRef = useRef<any>(new DOMMatrix())
-  const modelMatrixRef = useRef<any>(new DOMMatrix())
-  const modelAngleRef = useRef<number>(0)
-  const containerRef = useRef<any>(null)
+  const [isPlayGame, setIsPlayGame] = useState(false)
+  const [isPauseGame, setIsPauseGame] = useState(false)
+  const [entityList, setEntityList] = useState<EntityInfo[]>([])
+  const [particleEntityList, setParticleEntityList] = useState<Particle[]>([])
+  const entityListRef = useRef<EntityInfo[]>([])
+  const particleEntityListRef = useRef<Particle[]>([])
+  const [boxSize] = useState({width:0.05, height:0.05, depth:0.05, cornerRadius:0.01})
+  const [material] = useState({left:'matGreen', right:'matRed'})
+  const requestRef = useRef<number | null>(null);
+  const frameRef = useRef<number>(0)
+  const moveSpeed = 0.003
+  const pauseGameRef = useRef<boolean>(false)
 
-  const initDynamic3D = async () => {
-    let container = await callNative('CreateSpatializedDynamic3DElement', JSON.stringify({test:true}))
-    if(container.id){
-      containerRef.current = container.id
-      await callNative('AddSpatializedElementToSpatialScene', JSON.stringify({spatializedElementId:container.id}))
-
-      
-      let entity = await createBoxEntity(0.2, 0.1, 0.1, 0.02, "#FF0000")
-      if(entity.id){
-        await callNative('AddEntityToDynamic3D', JSON.stringify({dynamic3dId:container.id, entityId:entity.id}))
-      }
-
-      let entity2 = await createBoxEntity(0.1, 0.2, 0.1, 0.05, "#00FF00")
-      if(entity2.id){
-        await callNative('AddEntityToEntity', JSON.stringify({parentId:entity.id, childId:entity2.id}))
-
-        matrixRef.current.translateSelf(0, 0.1, 0)
-        matrixRef.current.rotateAxisAngleSelf(0,0,1,angleRef.current)
-        entityRef.current = entity2
-        console.log(entity2)
-        await callNative('UpdateEntityProperties', JSON.stringify({entityId:entity2.id, transform:matrixRef.current.toFloat64Array()}))
-        updateEntity()
-        createModelEntity()
-      }
-    }
-
-
+  const onClick = () => {
+    console.log('start game')
+    setIsPlayGame(true)
+    frameRef.current = 0
   }
 
-  const updateEntity = async () => {
-    if(entityRef.current){
-      matrixRef.current.rotateAxisAngleSelf(0,0,1,1)
-      await callNative('UpdateEntityProperties', JSON.stringify({entityId:entityRef.current.id, transform:matrixRef.current.toFloat64Array()}))
-
-    }
-    if(modelEntityRef.current){
-      modelAngleRef.current += 1
-      let posX = Math.sin(modelAngleRef.current * Math.PI / 180) * 0.2
-      let posY = Math.cos(modelAngleRef.current * Math.PI / 180) * 0.2
-      let matrix = new DOMMatrix()
-      matrix.translateSelf(posX, posY, 0)
-      matrix.rotateAxisAngleSelf(0,0,1,-modelAngleRef.current - 90)
-      modelMatrixRef.current = matrix
-      await callNative('UpdateEntityProperties', JSON.stringify({entityId:modelEntityRef.current, transform:modelMatrixRef.current.toFloat64Array()}))
-    }
-    requestAnimationFrame(updateEntity)
+  const onTapLeft = async (e: SpatialTapEvent) => {
+    console.log('tap left')
+    findAndRemoveNearestEntity('left')
   }
 
-  const createBoxEntity = async (width:number, height:number, depth:number, cornerRadius:number, color:string) => {
-    let geometryData = {
-      type: "BoxGeometry",
-      width,
-      height,
-      depth,
-      cornerRadius
-    }
-    let geometry = await callNative('CreateGeometry', JSON.stringify(geometryData))
-    let material = await callNative('CreateUnlitMaterial', JSON.stringify({
-      type: "UnlitMaterial",
-      color,
-    }))
-    let component = await callNative('CreateModelComponent', JSON.stringify({
-      type: "ModelComponent",
-      geometryId: geometry.id,
-      materialIds: [material.id],
-    }))
-    let entity = await callNative('CreateSpatialEntity', JSON.stringify({name:"entity"}))
-    await callNative('AddComponentToEntity', JSON.stringify({entityId:entity.id, componentId:component.id}))
-    return entity
+  const onTapRight = async (e: SpatialTapEvent) => {
+    console.log('tap right')
+    findAndRemoveNearestEntity('right')
   }
 
-  const createModelEntity = async () => {
-    let modelAsset = await callNative("CreateModelAsset", JSON.stringify({url:"http://localhost:5173/public/assets/RocketToy1.usdz"}))
-    if(modelAsset.id){
-      let entity = await callNative("CreateSpatialModelEntity", JSON.stringify({modelAssetId:modelAsset.id}))
-      if(entity.id){
-        await callNative('AddEntityToDynamic3D', JSON.stringify({dynamic3dId:containerRef.current, entityId:entity.id}))
-        let matrix = new DOMMatrix()
-        matrix.translateSelf(-0.1, 0, 0)
-        matrix.rotateAxisAngleSelf(0,0,1,-90)
-        await callNative('UpdateEntityProperties', JSON.stringify({entityId:entity.id, transform:matrix.toFloat64Array()}))
-        modelEntityRef.current = entity.id
+  const findAndRemoveNearestEntity = (side: 'left' | 'right') => {
+    let nearestEntity: EntityInfo | null = null
+    const sideEntities = entityListRef.current.filter(entity => entity.side === side)
+    if(sideEntities.length){
+      let dist = 0.4 - sideEntities[0].position.z
+      if(dist > -boxSize.depth && dist < boxSize.depth){
+        nearestEntity = sideEntities[0]
       }
     }
+    if(nearestEntity){
+      let list = entityListRef.current
+      list = list.filter(entity => entity.id !== nearestEntity.id)
+      let particle = new Particle({
+        layer: 4,
+        size: boxSize.width,
+        side: nearestEntity.side,
+        moveSpeed,
+        cornerRadius: boxSize.cornerRadius,
+        originPos: nearestEntity.position
+      })
+      setParticleEntityList(preList => [...preList, particle])
+      console.log("remove", entityList)
+      setEntityList(list)
+    }
   }
 
-  const callNative = async (command: string, data: string) => {
-    return await window?.webkit?.messageHandlers?.bridge?.postMessage(`${command}::${data}`)
+  const createBox = () => {
+    let rnd = Math.random() > .5
+    let entityInfo:EntityInfo = {
+      id: Math.random().toString(),
+      side: rnd ? 'left' : 'right',
+      position: {
+        x: rnd ? -0.2 : 0.2,
+        y: 0,
+        z: 0
+      },
+      materials: [rnd ? material.left : material.right]
+    }
+    console.log("create", entityList)
+    return entityInfo
   }
 
   useEffect(() => {
-    initDynamic3D()
+    entityListRef.current = entityList
+    console.log("update", entityList)
+  }, [entityList])
+
+  useEffect(() => {
+    pauseGameRef.current = isPauseGame
+  }, [isPauseGame])
+
+  useEffect(() => {
+    particleEntityListRef.current = particleEntityList
+  }, [particleEntityList])
+
+  useEffect(() => {
+    if(isPlayGame){
+      console.log("start update game")
+      function updateGame() {
+        if(pauseGameRef.current){
+          return
+        }
+        frameRef.current++
+        if(frameRef.current % 60 === 0){
+          let entityInfo = createBox()
+          entityListRef.current.push(entityInfo)
+        }
+        if(entityListRef.current.length){
+          let list = entityListRef.current.map(entity => ({
+            ...entity,
+            position: {
+              ...entity.position,
+              z: entity.position.z + moveSpeed
+            }
+          }))
+          list = list.filter(entity => entity.position.z < 0.45)
+          setEntityList(list)
+        }
+        if(particleEntityListRef.current.length){
+          let list = particleEntityListRef.current.map(particle => {
+            let isAlive = particle.update()
+            return isAlive ? particle : null
+          }).filter(Boolean) as Particle[]
+          setParticleEntityList(list)
+        }
+        requestRef.current = requestAnimationFrame(updateGame)
+      }
+      updateGame()
+    }
+    else{
+      if(requestRef.current){
+        cancelAnimationFrame(requestRef.current)
+        requestRef.current = null
+      }
+    }
+  }, [isPlayGame])
+
+  useEffect(() => {
+    return () => {
+      if(requestRef.current){
+        cancelAnimationFrame(requestRef.current)
+      }
+    }
   }, [])
 
   return (
-    <>
-      <div style={{ width: '100px', height: '100px' }}>
-        Start of SpatializedContainer
-      </div>
-    </>
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      background:"transparent",
+      "--xr-background-material": "translucent",
+    }}>
+      { !isPlayGame && <>
+        <div style={{color:"white", fontSize:"70px", fontWeight:"bold"}}>Let's Saber!</div>
+        <div style={{padding: "2em"}}>
+          <button style={{marginTop:"40px", width:"200px", height:"80px", fontSize:"30px"}} onClick={onClick}>
+            Start!
+          </button>
+        </div>
+      </> }
+      { isPlayGame && <>
+        <button onClick={() => setIsPauseGame(!isPauseGame)}>
+          {isPauseGame ? 'Resume' : 'Pause'}
+        </button>
+        <Reality
+          style={{
+            width: "100vw",
+            height: "100vh"
+          }}
+        >
+          <UnlitMaterial id={material.right} color="#ff0000" />
+          <UnlitMaterial id={material.left} color="#00ff00" />
+          <SceneGraph>
+            <BoxEntity
+              width={boxSize.width}
+              height={boxSize.height}
+              depth={0.01}
+              cornerRadius={boxSize.cornerRadius}
+              materials={[material.right]}
+              position={{ x: 0.2, y: 0, z: 0.4 }}
+              onSpatialTap={onTapRight}
+             />
+            <BoxEntity
+              width={boxSize.width}
+              height={boxSize.height}
+              depth={0.01}
+              cornerRadius={boxSize.cornerRadius}
+              materials={[material.left]}
+              position={{ x: -0.2, y: 0, z: 0.4 }}
+              onSpatialTap={onTapLeft}
+             />
+             {entityList.map((entityInfo, index) => (
+               <BoxEntity
+                key={index}
+                width={boxSize.width}
+                height={boxSize.height}
+                depth={boxSize.depth}
+                cornerRadius={boxSize.cornerRadius}
+                materials={entityInfo.materials}
+                position={entityInfo.position}
+               />
+             ))}
+             {
+              particleEntityList.map((particle, index) => {
+                return particle.getPartDatas().map(partData => (
+                  <BoxEntity
+                    key={partData.id}
+                    width={boxSize.width}
+                    height={boxSize.height}
+                    depth={boxSize.depth}
+                    cornerRadius={boxSize.cornerRadius}
+                    materials={partData.materials}
+                    position={partData.position}
+                    scale={partData.scale}
+                  />
+                ))
+              })
+             }
+          </SceneGraph>
+        </Reality>
+      </> }
+    </div>
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+// Initialize react
+var root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.createRoot(root).render(
+  // todo: add strict mode to check destroy
+  <React.StrictMode>
+    <App />,
+  </React.StrictMode>,
+)
+document.documentElement.style.backgroundColor = 'transparent'
+document.body.style.backgroundColor = 'transparent'
+// Force page height to 100% to get centering to work
+document.documentElement.style.height = '100%'
+document.body.style.height = '100%'
+root.style.height = '100%'
