ffd639ab8d4322dc408ee47a46956098ba0fd9f2
wangyang <wangyang.dev@bytedance.com>
2025-09-19 14:34:44 +0800

    reality: remove createReality cache

--- packages/react/src/reality/components/Reality.tsx
+++ packages/react/src/reality/components/Reality.tsx
@@ -19,55 +19,56 @@ type Props = {
 export const Reality = forwardRef<SpatializedElementRef, Props>(
   function RealityBase({ children, ...props }, ref) {
     const ctxRef = useRef<RealityContextValue>(null)
+
+    const isCancelled = useRef(false)
+
     const [isReady, setIsReady] = useState(false)
     useEffect(() => {
-      let cancelled = false
+      isCancelled.current = false
 
-      const init = async () => {
-        const resourceRegistry = new ResourceRegistry()
-        const session = await getSession()
-        if (!session) {
-          console.error('getSession failed')
-          return
-        }
-        const reality = await session.createSpatializedDynamic3DElement()
-        if (cancelled) {
-          reality.destroy()
-          return
-        }
-        await session.getSpatialScene().addSpatializedElement(reality)
-        ctxRef.current = { session, reality, resourceRegistry }
-        setIsReady(true)
-      }
-      init()
       return () => {
-        cancelled = true
+        isCancelled.current = true
         ctxRef.current?.resourceRegistry.destroy()
         ctxRef.current?.reality.destroy()
+        ctxRef.current = null
+        setIsReady(false)
       }
     }, [])
 
-    const getReality = useCallback(
-      () => Promise.resolve(ctxRef.current?.reality!),
-      [],
-    )
+    const createReality = useCallback(async () => {
+      const resourceRegistry = new ResourceRegistry()
+      const session = await getSession()
+      if (!session) {
+        console.error('getSession failed')
+        return
+      }
+      const reality = await session.createSpatializedDynamic3DElement()
+      if (isCancelled.current) {
+        reality.destroy()
+        resourceRegistry.destroy()
+        setIsReady(false)
+        return
+      }
+      await session.getSpatialScene().addSpatializedElement(reality)
+      ctxRef.current = { session, reality, resourceRegistry }
+      setIsReady(true)
+      return reality
+    }, [])
 
     const content = useCallback(({ spatializedElement, ...rest }: any) => {
       return <></>
     }, [])
-
-    if (!isReady) return null
 
     return (
       <RealityContext.Provider value={ctxRef.current}>
         <SpatializedContainer<SpatializedElementRef>
           component={'div'}
           ref={ref}
-          createSpatializedElement={getReality}
+          createSpatializedElement={createReality}
           spatializedContent={content}
           {...props}
         />
-        {children}
+        {isReady && children}
       </RealityContext.Provider>
     )
   },
