c5b0f44e6cf66d8bc727c794ab4a431a0b182745
wangyang <wangyang.dev@bytedance.com>
2025-09-24 13:11:40 +0800

    chore: log detail of erroMessage

--- packages/core/src/reality/realityCreator.ts
+++ packages/core/src/reality/realityCreator.ts
@@ -24,7 +24,7 @@ export async function createSpatialEntity(
 ): Promise<SpatialEntity> {
   const result = await new CreateSpatialEntityCommand(name).execute()
   if (!result.success) {
-    throw new Error('createSpatialEntity failed')
+    throw new Error('createSpatialEntity failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new SpatialEntity(id, name)
@@ -40,7 +40,7 @@ export async function createSpatialGeometry<T extends SpatialGeometry>(
     options,
   ).execute()
   if (!result.success) {
-    throw new Error('createSpatialGeometry failed')
+    throw new Error('createSpatialGeometry failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new ctor(id, options) as T
@@ -52,7 +52,7 @@ export async function createSpatialUnlitMaterial(
 ) {
   const result = await new CreateSpatialUnlitMaterialCommand(options).execute()
   if (!result.success) {
-    throw new Error('createSpatialUnlitMaterial failed')
+    throw new Error('createSpatialUnlitMaterial failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new SpatialUnlitMaterial(id, options)
@@ -62,7 +62,7 @@ export async function createSpatialUnlitMaterial(
 export async function createModelComponent(options: ModelComponentOptions) {
   const result = await new CreateModelComponentCommand(options).execute()
   if (!result.success) {
-    throw new Error('createModelComponent failed')
+    throw new Error('createModelComponent failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new ModelComponent(id, options)
@@ -74,7 +74,7 @@ export async function createSpatialModelEntity(
 ) {
   const result = await new CreateSpatialModelEntityCommand(options).execute()
   if (!result.success) {
-    throw new Error('createSpatialModelEntity failed')
+    throw new Error('createSpatialModelEntity failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new SpatialModelEntity(id)
@@ -84,7 +84,7 @@ export async function createSpatialModelEntity(
 export async function createModelAsset(options: ModelAssetOptions) {
   const result = await new CreateModelAssetCommand(options).execute()
   if (!result.success) {
-    throw new Error('createModelAsset failed')
+    throw new Error('createModelAsset failed:' + result?.errorMessage)
   } else {
     const { id } = result.data
     return new SpatialModelAsset(id, options)
