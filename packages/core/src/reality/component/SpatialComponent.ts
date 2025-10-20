72ee8e8770b6f23a4afbc908095d7929d57787d0
wangyang <wangyang.dev@bytedance.com>
2025-10-20 15:32:29 +0800

    fix destroy should correctly set mark of entity and component

--- packages/core/src/reality/component/SpatialComponent.ts
+++ packages/core/src/reality/component/SpatialComponent.ts
@@ -1,7 +1,17 @@
 import { SpatialObject } from '../../SpatialObject'
+import { SpatialWebEvent } from '../../SpatialWebEvent'
+import { ObjectDestroyMsg, SpatialWebMsgType } from '../../WebMsgCommand'
 
 export class SpatialComponent extends SpatialObject {
   constructor(id: string) {
     super(id)
+    SpatialWebEvent.addEventReceiver(id, this.onReceiveEvent)
+  }
+
+  private onReceiveEvent = (data: ObjectDestroyMsg) => {
+    const { type } = data
+    if (type === SpatialWebMsgType.objectdestroy) {
+      this.isDestroyed = true
+    }
   }
 }
