aa6d35598de9fa91df1b600f912b038548268a85
Siyaman <siyaman.sadagopan@bytedance.com>
2025-10-21 12:34:28 +0800

    Add Android support

--- packages/core/src/platform-adapter/index.ts
+++ packages/core/src/platform-adapter/index.ts
@@ -1,6 +1,10 @@
 import { VisionOSPlatform } from './vision-os/VisionOSPlatform'
 import { PlatformAbility } from './interface'
+import { AndroidPlatform } from './android/AndroidPlatform'
 
 export function createPlatform(): PlatformAbility {
+  if (window.navigator.userAgent.includes('Android')) {
+    return new AndroidPlatform()
+  }
   return new VisionOSPlatform()
 }
