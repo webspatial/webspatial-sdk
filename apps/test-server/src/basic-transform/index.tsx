d1b3baa4e424ed2bf6eaab2416094466486d4aee
walter.wang <wangren.walter@bytedance.com>
2025-11-06 17:27:25 +0800

    for reproduce clip issue in andorid

--- apps/test-server/src/basic-transform/index.tsx
+++ apps/test-server/src/basic-transform/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 
-import { enableDebugTool, Model } from '@webspatial/react-sdk'
+import { enableDebugTool } from '@webspatial/react-sdk'
+import { CSSProperties } from 'react'
 
 enableDebugTool()
 
@@ -8,14 +9,29 @@ function App() {
   const style = {
     width: '300px',
     height: '300px',
-    // '--xr-back': 100,
+    '--xr-back': 100,
     // transform: 'rotateX(30deg)',
     backgroundColor: 'green',
+  }
+
+  const childStyle: CSSProperties = {
+    width: '300px',
+    height: '100px',
+    position: 'relative',
+    left: '120px',
+    '--xr-back': 20,
+    backgroundColor: 'red',
+    // transform: '  rotateY(30deg)',
   }
   return (
     <div>
       hello basic-transform
-      <div enable-xr style={style} />
+      <div enable-xr style={style}>
+        parent spatial div
+        <div enable-xr style={childStyle}>
+          this is child spatialdiv tom
+        </div>
+      </div>
       tail end
     </div>
   )
--- packages/visionOS/web-spatial/manifest.swift
+++ packages/visionOS/web-spatial/manifest.swift
@@ -5,7 +5,7 @@ var pwaManager = PWAManager()
 
 struct PWAManager: Codable {
     var isLocal: Bool = false
-    var start_url: String = "http://localhost:5173/src/spatial-transform/"
+    var start_url: String = "http://localhost:5173/src/basic-transform/"
 
     var scope: String = ""
     var id: String = "com.webspatial.pico"
