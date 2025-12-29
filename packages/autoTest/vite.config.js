8554fdbd8d97b01796647a36ff411764d142171e
kuhat <1043744621@qq.com>
2025-12-29 19:05:26 +0800

    decouple react & react-dom for autoTestPacakge

--- packages/autoTest/vite.config.js
+++ packages/autoTest/vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({})
+export default defineConfig({
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+})
