2867ffd345d311d3913647781919da2b88fc1638
Parth Patel <parth.p@bytedance.com>
2026-02-05 15:34:40 -0800

    update vercel deployment manifest

--- apps/test-server/index.tsx
+++ apps/test-server/index.tsx
@@ -66,6 +66,20 @@ function App() {
   )
 }
 
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration)
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
+  })
+}
+
 const init = () => {
   const rootElement = document.getElementById('root')
   if (!rootElement) {
