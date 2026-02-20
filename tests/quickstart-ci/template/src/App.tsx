29ad9cee70cbc7fbc163380579120f5b87b3b6a2
Parth Patel <parth.p@bytedance.com>
2026-02-19 17:45:15 -0800

    Add modular CI validation script for WebSpatial Quickstart
    create a quickstart-ci to make sure developer path quick start is working

---
+++ tests/quickstart-ci/template/src/App.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="my-spatial-card __enableXr__">
+      <h1>🚀 Hello WebSpatial!</h1>
+      <p>I am floating 150 units in front of the window.</p>
+    </div>
+  )
+}
