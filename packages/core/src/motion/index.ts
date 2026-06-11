ce1d4574824d22054f0d49b6fcfb5d9dd0bb94dd
wangyang <wangyang.dev@bytedance.com>
2026-06-11 17:08:42 +0800

    refactor remove handle

--- packages/core/src/motion/index.ts
+++ packages/core/src/motion/index.ts
@@ -2,7 +2,6 @@ export * from './compute'
 export * from './control'
 export { parseSpatializedVisualValues } from './native/parseSpatializedVisualValues'
 export { motionConfigToNativeTimeline } from './native'
-export type { SpatializedMotionHandle } from './control/SpatializedMotionHandle'
 export {
   SpatializedMotionController,
   type SpatializedMotionControllerOptions,
