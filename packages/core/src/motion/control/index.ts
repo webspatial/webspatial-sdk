b0020498dd2a953cb63bd3434f3bf4b3490dcdbd
wangyang <wangyang.dev@bytedance.com>
2026-06-11 14:41:45 +0800

    refactor(core): simplify motion playback internals

--- packages/core/src/motion/control/index.ts
+++ packages/core/src/motion/control/index.ts
@@ -4,9 +4,3 @@ export {
 } from './SpatializedMotionController'
 export type { SpatializedMotionHandle } from './SpatializedMotionHandle'
 export { MOTION_KIND_POLICIES } from './motionKindPolicy'
-
-export {
-  evaluateMotionTimeline,
-  validateSpatializedMotionConfig as validateSpatializedMotionConfig,
-  segmentConfigToMotionConfig as segmentConfigToMotionConfig,
-} from '../compute'
