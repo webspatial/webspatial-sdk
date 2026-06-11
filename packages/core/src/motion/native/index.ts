3c126357bd2e4b584d6075cf15078cd4392800e3
wangyang <wangyang.dev@bytedance.com>
2026-06-11 17:30:56 +0800

    clean up

--- packages/core/src/motion/native/index.ts
+++ packages/core/src/motion/native/index.ts
@@ -1,7 +1,3 @@
 export { parseSpatializedVisualValues } from './parseSpatializedVisualValues'
 export { executeAnimateSpatializedElementMotion } from './executeAnimateSpatializedElementMotion'
 export { motionConfigToNativeTimeline } from './serializeMotionTimeline'
-export {
-  removeMotionEventReceivers,
-  MOTION_EVENT_SUFFIXES,
-} from './motionEventReceivers'
