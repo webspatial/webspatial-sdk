33267a785742c8f77cb6703e0c1fa9ca20f03aba
wangyang <wangyang.dev@bytedance.com>
2026-06-11 15:54:25 +0800

    refactor(core): narrow motion native serializer exports

--- packages/core/src/motion/native/index.ts
+++ packages/core/src/motion/native/index.ts
@@ -1,5 +1,6 @@
 export { parseSpatializedVisualValues } from './parseSpatializedVisualValues'
 export { executeAnimateSpatializedElementMotion } from './executeAnimateSpatializedElementMotion'
+export { motionConfigToNativeTimeline } from './serializeMotionTimeline'
 export {
   removeMotionEventReceivers,
   MOTION_EVENT_SUFFIXES,
