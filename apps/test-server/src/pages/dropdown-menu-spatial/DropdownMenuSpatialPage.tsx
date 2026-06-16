663bb77394ed47cafc50289fbc60733906e30de7
walter.wang <wangren.walter@bytedance.com>
2026-06-16 17:14:39 +0800

    test-server: show scenario 4 plugin host demo

--- apps/test-server/src/pages/dropdown-menu-spatial/DropdownMenuSpatialPage.tsx
+++ apps/test-server/src/pages/dropdown-menu-spatial/DropdownMenuSpatialPage.tsx
@@ -46,7 +46,7 @@ export function DropdownMenuSpatialPage() {
       </div>
 
       <section style={{ marginTop: '24px' }}>
-        {/* <Scenario4PluginHostMenu onLog={logPluginHost} /> */}
+        <Scenario4PluginHostMenu onLog={logPluginHost} />
       </section>
 
       <section style={{ marginTop: '24px' }}>
